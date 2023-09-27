6073d21ef56a9ea2409dacea03ae06929d7b18fe
pcheremu <pavel.cheremushkin@gmail.com>
2023-09-27 17:04:04 +0200

    test: refactoring of the test solution structure and naming

--- packages/bets/.eslintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ["@matterlabs/eslint-config-vue"],
-};
--- packages/bets/tests/e2e/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  mainWindowSize: { width: 1920, height: 1080 },
-  headless: true,
-  slowMo: 10,
-  defaultTimeout: { timeout: 5000 },
-  increasedTimeout: { timeout: 17000 },
-  extraTimeout: { timeout: 60000 },
-};
