03c1e8fddc71e5f8fa2b730e41f7cd34d5cb98b7
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-10-02 18:15:00 +0300

    fix: format MyNFT.sol

--- packages/integration-tests/src/playbook/contracts/MyNFT.sol
+++ packages/integration-tests/src/playbook/contracts/MyNFT.sol
@@ -7,22 +7,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable {
-using Counters for Counters.Counter;
-Counters.Counter private _tokenIds;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-constructor() ERC721("MyNFT", "NFT") {}
+  constructor() ERC721("MyNFT", "NFT") {}
 
-function mintNFT(address recipient, string memory tokenURI)
-public
-onlyOwner
-returns (uint256)
-{
-_tokenIds.increment();
+  function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    _tokenIds.increment();
 
-uint256 newItemId = _tokenIds.current();
-_mint(recipient, newItemId);
-_setTokenURI(newItemId, tokenURI);
+    uint256 newItemId = _tokenIds.current();
+    _mint(recipient, newItemId);
+    _setTokenURI(newItemId, tokenURI);
 
-return newItemId;
-}
+    return newItemId;
+  }
 }
