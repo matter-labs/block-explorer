c9b22626c484c6c93fa5dfb278b55867f28504e7
pcheremu <pavel.cheremushkin@gmail.com>
2023-10-05 16:52:12 +0200

    test: use pre-seed instead of separate sh script

--- packages/integration-tests/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Test run"
-npx jest --verbose --testPathPattern=tokens.test.ts
-npx jest --verbose --testPathPattern=deposit.test.ts
-npx jest --verbose --testPathPattern=common
-npx jest --verbose --testPathPattern=transactions
