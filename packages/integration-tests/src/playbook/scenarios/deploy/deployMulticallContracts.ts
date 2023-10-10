dc2b9f6d7a2b08c7124e733ef46a9f7a20ab7d2e
Roman Petriv <petriv.roma@gmail.com>
2023-10-10 15:57:52 +0300

    feat: integration tests setup (#45)

---
+++ packages/integration-tests/src/playbook/scenarios/deploy/deployMulticallContracts.ts
@@ -0,0 +1,28 @@
+import { Buffer } from "../../../entities";
+import { Helper } from "../../../helper";
+
+export const deployMulticallContracts = async function () {
+  const helper = new Helper();
+  const playbookRoot = "src/playbook";
+  const bufferFileRoot = playbookRoot + "/" + Buffer.addressMultiCallRoot;
+  const bufferFileMiddle = playbookRoot + "/" + Buffer.addressMultiCallMiddle;
+  const bufferFileCaller = playbookRoot + "/" + Buffer.addressMultiCallCaller;
+
+  await helper.executeScript(`cd ${playbookRoot} && npm run compile`);
+  await helper.executeScript(`cd ${playbookRoot} && npm run deploy:multicall:root`);
+  await helper.executeScript(`cd ${playbookRoot} && npm run deploy:multicall:middle`);
+  await helper.executeScript(`cd ${playbookRoot} && npm run deploy:multicall:caller`);
+
+  const multiCallRoot = await helper.getStringFromFile(bufferFileRoot);
+  const multiCallMiddle = await helper.getStringFromFile(bufferFileMiddle);
+  const multiCallCaller = await helper.getStringFromFile(bufferFileCaller);
+
+  console.log(
+    "Multicall contracts (Root, Middle and Caller) have been deployed to the address: ",
+    multiCallRoot,
+    multiCallMiddle,
+    multiCallCaller
+  );
+
+  return [multiCallRoot, multiCallMiddle, multiCallCaller];
+};
