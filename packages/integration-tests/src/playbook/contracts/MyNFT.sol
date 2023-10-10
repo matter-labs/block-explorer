25409aed6b638f40f80258e14bf0e7737090f497
Roman Petriv <petriv.roma@gmail.com>
2023-10-10 15:57:52 +0300

    feat: integration tests setup (#45)

---
+++ packages/integration-tests/src/playbook/contracts/MyNFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("MyNFT", "NFT") {}
+
+  function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _mint(recipient, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    return newItemId;
+  }
+}
