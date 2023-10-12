edaef9d5b03b6271f1cd5b6c5d4615b133de4be5
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-10-12 12:29:54 +0300

    fix: separate app configs for local and dev envs

--- packages/app/src/composables/useEnvironmentConfig.ts
+++ packages/app/src/composables/useEnvironmentConfig.ts
@@ -5,7 +5,7 @@ import type { EnvironmentConfig, NetworkConfig } from "@/configs";
 const config = ref<EnvironmentConfig | null>(null);
 
 const HYPERCHAIN_CONFIG_NAME = "hyperchain";
-const LOCAL_CONFIG_NAME = "local";
+const DEVELOPMENT_CONFIG_NAME = "dev";
 
 export async function loadEnvironmentConfig(appEnvironment: string): Promise<void> {
   let envConfig: EnvironmentConfig;
@@ -13,7 +13,7 @@ export async function loadEnvironmentConfig(appEnvironment: string): Promise<voi
     try {
       envConfig = (await import(`../configs/${HYPERCHAIN_CONFIG_NAME}.config.json`)).default;
     } catch {
-      envConfig = (await import(`../configs/${LOCAL_CONFIG_NAME}.config.json`)).default;
+      envConfig = (await import(`../configs/${DEVELOPMENT_CONFIG_NAME}.config.json`)).default;
     }
   } else {
     envConfig = (await import(`../configs/${appEnvironment}.config.json`)).default;
