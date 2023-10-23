8ae77e8ba86a242d5ed9302abec39a99b3dec76c
Roman Petriv <petriv.roma@gmail.com>
2023-10-23 15:59:30 +0300

    fix: why paymaster label

--- packages/app/tests/components/FeeData.spec.ts
+++ packages/app/tests/components/FeeData.spec.ts
@@ -243,7 +243,7 @@ describe("FeeToken", () => {
         "https://era.zksync.io/docs/dev/developer-guides/transactions/fee-model.html#refunds"
       );
       expect(refundedLink?.getAttribute("target")).toBe("_blank");
-      expect(refundedLink?.textContent).toBe("Why Paymaster is being refunded?");
+      expect(refundedLink?.textContent).toBe("Why is Paymaster being refunded?");
 
       const paymasterLink = container.querySelector(".paymaster-link");
       expect(paymasterLink?.getAttribute("href")).toBe(
