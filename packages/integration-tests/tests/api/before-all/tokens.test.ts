6a582e57c489638e7daa192490ac3cd2afa95d7d
abilevych <anton@matterlabs.dev>
2023-10-24 14:31:31 +0300

    test: restructure test solution

--- packages/integration-tests/tests/api/before-all/tokens.test.ts
+++ packages/integration-tests/tests/api/before-all/tokens.test.ts
@@ -20,25 +20,6 @@ describe("Tokens", () => {
       expect(deployedToken).toContain(Logger.txHashStartsWith);
     });
 
-    //@id1456
-    it("Verify deployed to L2 custom token via /tokens/{tokenAddress}", async () => {
-      await setTimeout(localConfig.extendedPause); //works unstable without timeout
-      const apiRoute = `/tokens/${deployedToken}`;
-
-      return request(environment.blockExplorerAPI)
-        .get(apiRoute)
-        .expect(200)
-        .expect((res) =>
-          expect(res.body).toStrictEqual({
-            l2Address: deployedToken,
-            l1Address: null,
-            symbol: Token.customL2TokenSymbol,
-            name: Token.customL2TokenName,
-            decimals: Token.customL2TokenDecimals,
-          })
-        );
-    });
-
     //@id657
     it("Deploy custom ERC20 token to L1", async () => {
       deployedToken = await playbook.deployERC20toL1();
