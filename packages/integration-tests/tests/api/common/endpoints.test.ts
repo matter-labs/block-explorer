00f292ffd8fd89c6398a00356fbc2eefc71b828e
abilevych <anton@matterlabs.dev>
2023-10-24 14:31:31 +0300

    test: restructure test solution

--- packages/integration-tests/tests/api/common/endpoints.test.ts
+++ packages/integration-tests/tests/api/common/endpoints.test.ts
@@ -2,12 +2,12 @@ import * as request from "supertest";
 import { setTimeout } from "timers/promises";
 
 import { environment, localConfig } from "../../../src/config";
-import { Buffer } from "../../../src/entities";
-import { Helper } from "../../../src/helper";
+// import { Buffer } from "../../../src/entities";
+// import { Helper } from "../../../src/helper";
 
 describe("Endpoints", () => {
-  const helper = new Helper();
-  const bufferRoute = "src/playbook/";
+  // const helper = new Helper();
+  // const bufferRoute = "src/playbook/";
 
   jest.setTimeout(localConfig.extendedTimeout);
 
@@ -26,45 +26,6 @@ describe("Endpoints", () => {
         .expect((res) => expect(typeof res.body.lastSealedBlock).toStrictEqual("number"))
         .expect((res) => expect(typeof res.body.lastVerifiedBlock).toStrictEqual("number"))
         .expect((res) => expect(typeof res.body.totalTransactions).toStrictEqual("number"));
-    });
-  });
-
-  describe("/tokens", () => {
-    //@id1508
-    it("Verify the response via /tokens", async () => {
-      const l2DepositedToken = await helper.getStringFromFile(bufferRoute + Buffer.L2deposited);
-      const l1Token = await helper.getStringFromFile(bufferRoute + Buffer.L1);
-      const l2Token = await helper.getStringFromFile(bufferRoute + Buffer.L2);
-
-      const apiRoute = `/tokens`;
-
-      await setTimeout(localConfig.standardPause); //works unstable without timeout
-
-      return request(environment.blockExplorerAPI)
-        .get(apiRoute)
-        .expect(200)
-        .expect((res) => expect(Array.isArray(res.body.items)).toStrictEqual(true))
-        .expect((res) =>
-          expect(res.body.items[0]).toStrictEqual(expect.objectContaining({ l2Address: l2DepositedToken }))
-        )
-        .expect((res) => expect(res.body.items[0]).toStrictEqual(expect.objectContaining({ l1Address: l1Token })))
-        .expect((res) => expect(res.body.items[0]).toStrictEqual(expect.objectContaining({ symbol: "L1" })))
-        .expect((res) => expect(res.body.items[0]).toStrictEqual(expect.objectContaining({ name: "L1 ERC20 token" })))
-        .expect((res) => expect(res.body.items[0]).toStrictEqual(expect.objectContaining({ decimals: 18 })))
-        .expect((res) => expect(res.body.items[1]).toStrictEqual(expect.objectContaining({ l2Address: l2Token })))
-        .expect((res) => expect(res.body.items[1]).toStrictEqual(expect.objectContaining({ l1Address: null })))
-        .expect((res) => expect(res.body.items[1]).toStrictEqual(expect.objectContaining({ symbol: "L2" })))
-        .expect((res) => expect(res.body.items[1]).toStrictEqual(expect.objectContaining({ name: "L2 ERC20 token" })))
-        .expect((res) => expect(res.body.items[1]).toStrictEqual(expect.objectContaining({ decimals: 18 })))
-        .expect((res) => expect(typeof res.body.meta.totalItems).toStrictEqual("number"))
-        .expect((res) => expect(typeof res.body.meta.itemCount).toStrictEqual("number"))
-        .expect((res) => expect(typeof res.body.meta.itemsPerPage).toStrictEqual("number"))
-        .expect((res) => expect(typeof res.body.meta.totalPages).toStrictEqual("number"))
-        .expect((res) => expect(typeof res.body.meta.currentPage).toStrictEqual("number"))
-        .expect((res) => expect(res.body.links.first).toStrictEqual("tokens?limit=10"))
-        .expect((res) => expect(res.body.links.previous).toStrictEqual(""))
-        .expect((res) => expect(typeof res.body.links.next).toStrictEqual("string"))
-        .expect((res) => expect(typeof res.body.links.last).toStrictEqual("string"));
     });
   });
 
