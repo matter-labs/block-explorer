6d2a4037828e485cca6d5c66b51ef00e8c03f45f
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-11-03 11:02:21 +0200

    feat: app to support realtime environment config (#78)
    # What ❔
    
    Adjust app so it supports realtime environment config
    
    ## Why ❔
    
    We need this feature to set different app configs from the CLI
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [X] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [X] Tests for the changes have been added / updated.

--- packages/app/src/composables/useEnvironmentConfig.ts
+++ packages/app/src/composables/useEnvironmentConfig.ts
@@ -1,22 +1,28 @@
 import { computed, ref } from "vue";
 
-import type { EnvironmentConfig, NetworkConfig } from "@/configs";
+import type { EnvironmentConfig, NetworkConfig, RuntimeConfig } from "@/configs";
 
 const config = ref<EnvironmentConfig | null>(null);
 
 const HYPERCHAIN_CONFIG_NAME = "hyperchain";
 const DEVELOPMENT_CONFIG_NAME = "dev";
 
-export async function loadEnvironmentConfig(appEnvironment: string): Promise<void> {
+export async function loadEnvironmentConfig(runtimeConfig: RuntimeConfig): Promise<void> {
+  // runtime environment config takes precedence over hard coded config
+  if (runtimeConfig.environmentConfig) {
+    config.value = runtimeConfig.environmentConfig;
+    return;
+  }
+
   let envConfig: EnvironmentConfig;
-  if (appEnvironment === "default") {
+  if (runtimeConfig.appEnvironment === "default") {
     try {
       envConfig = (await import(`../configs/${HYPERCHAIN_CONFIG_NAME}.config.json`)).default;
     } catch {
       envConfig = (await import(`../configs/${DEVELOPMENT_CONFIG_NAME}.config.json`)).default;
     }
   } else {
-    envConfig = (await import(`../configs/${appEnvironment}.config.json`)).default;
+    envConfig = (await import(`../configs/${runtimeConfig.appEnvironment}.config.json`)).default;
   }
   config.value = envConfig;
 }
--- packages/app/src/configs/index.ts
+++ packages/app/src/configs/index.ts
@@ -18,3 +18,10 @@ export type NetworkConfig = {
 export type EnvironmentConfig = {
   networks: NetworkConfig[];
 };
+
+export type RuntimeConfig = {
+  version: string;
+  sentryDSN: string;
+  appEnvironment: string;
+  environmentConfig?: EnvironmentConfig;
+};
