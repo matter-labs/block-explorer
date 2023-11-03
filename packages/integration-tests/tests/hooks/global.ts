61bb3bbb711c20e40f8aeaa4c9c65eb8f4d16961
abilevych <anton@matterlabs.dev>
2023-11-03 11:22:43 +0200

    chore: implemented a global hook for jest

--- packages/integration-tests/tests/hooks/global.ts
+++ packages/integration-tests/tests/hooks/global.ts
@@ -2,15 +2,22 @@ import { Buffer } from "../../src/entities";
 import { Helper } from "../../src/helper";
 import { Playbook } from "../../src/playbook/playbook";
 
-export default async () => {
+async function globalHook() {
   const playbook = new Playbook();
   const helper = new Helper();
   const bufferRoute = "src/playbook/";
+  const bufferFile = bufferRoute + Buffer.L1;
+  const token = await helper.getStringFromFile(bufferFile);
 
   await playbook.deployERC20toL2();
   await playbook.deployERC20toL1();
   await playbook.depositETH("0.0000001");
-  const bufferFile = bufferRoute + Buffer.L1;
-  const token = await helper.getStringFromFile(bufferFile);
   await playbook.depositERC20("100", token, 18);
-};
+}
+
+globalHook()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
