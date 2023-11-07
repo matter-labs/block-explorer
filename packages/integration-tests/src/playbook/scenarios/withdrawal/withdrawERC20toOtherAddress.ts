861c2121dfccc2939a49a9f70ae29daf584d83f9
abilevych <anton@matterlabs.dev>
2023-11-07 11:17:10 +0200

    chore: optimized addresses/transactions test suites

--- packages/integration-tests/src/playbook/scenarios/withdrawal/withdrawERC20toOtherAddress.ts
+++ packages/integration-tests/src/playbook/scenarios/withdrawal/withdrawERC20toOtherAddress.ts
@@ -1,8 +1,9 @@
 import * as ethers from "ethers";
+import { promises as fs } from "fs";
 import * as zksync from "zksync-web3";
 
 import { localConfig } from "../../../config";
-import { Logger, Wallets } from "../../../entities";
+import { Buffer, Logger, Wallets } from "../../../entities";
 import { Helper } from "../../../helper";
 
 export const withdrawERC20toOtherAddress = async function (tokenAddress: string, sum = "0.2") {
@@ -11,6 +12,8 @@ export const withdrawERC20toOtherAddress = async function (tokenAddress: string,
   const ethProvider = ethers.getDefaultProvider(localConfig.L1Network);
   const syncWallet = new zksync.Wallet(localConfig.privateKey, syncProvider, ethProvider);
   const bridges = await syncProvider.getDefaultBridgeAddresses();
+  const playbookRoot = "src/playbook/";
+  const bufferFile = playbookRoot + Buffer.txERC20WithdrawOtherAddress;
 
   const balance = await syncWallet.getBalance(tokenAddress);
 
@@ -34,6 +37,7 @@ export const withdrawERC20toOtherAddress = async function (tokenAddress: string,
   console.log(`Your balance is ${balanceAfter.toString()}`);
 
   await helper.txHashLogger(Logger.withdraw, txHash, "Custom token");
+  await fs.writeFile(bufferFile, txHash);
 
   return txHash;
 };
