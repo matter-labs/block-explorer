e97c6d832af0c5c39b52ec8a5a0ab420e837017b
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-11-09 14:38:10 +0200

    feat: add search route (#84)
    # What ❔
    
    App search route which redirects the request to tx, batch or address
    route depending on the param. Examples:
    ```
    /search?q=123 -> /batch/123
    
    /search?q=0x7e341fa7fa5368ece4814825fd181697404b5d243172db071bf5562c02976ad5 -> /tx/0x7e341fa7fa5368ece4814825fd181697404b5d243172db071bf5562c02976ad5
    
    /search?q=0x043927b4e501835556057472A5636e99fC247e9A -> /address/0x043927b4e501835556057472A5636e99fC247e9A
    ```
    
    ## Why ❔
    
    Unified search URL is requested by developers who work on integrations
    with zkSync.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [X] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [X] Tests for the changes have been added / updated.

--- packages/app/tests/views/BatchView.spec.ts
+++ packages/app/tests/views/BatchView.spec.ts
@@ -19,6 +19,14 @@ const router = {
   },
 };
 
+vi.mock("@/composables/useSearch", () => {
+  return {
+    default: () => ({
+      getSearchRoute: () => null,
+    }),
+  };
+});
+
 vi.mock("vue-router", () => ({
   useRouter: () => router,
   useRoute: () => vi.fn(),
@@ -43,7 +51,7 @@ describe("BatchView:", () => {
   });
 
   it("has correct title", async () => {
-    expect(i18n.global.t(routes.find((e) => e.name === "batch")?.meta.title as string)).toBe("Batch");
+    expect(i18n.global.t(routes.find((e) => e.name === "batch")?.meta?.title as string)).toBe("Batch");
   });
 
   it("route is replaced with not found view on request 404 error", async () => {
--- packages/app/tests/views/HomeView.spec.ts
+++ packages/app/tests/views/HomeView.spec.ts
@@ -53,7 +53,7 @@ describe("HomeView:", () => {
   };
 
   it("has correct title", async () => {
-    expect(i18n.global.t(routes.find((e) => e.name === "home")?.meta.title as string)).toBe(
+    expect(i18n.global.t(routes.find((e) => e.name === "home")?.meta?.title as string)).toBe(
       "Transactions, Blocks, Contracts and much more"
     );
   });
