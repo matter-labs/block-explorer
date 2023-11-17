978e5207213fd3de41ceb0241932199a11c80b69
amelnytskyi <a.melnytskyi@matterlabs.dev>
2023-11-17 12:23:25 +0200

    test: covering Contract API and Logs API endpoints

--- packages/integration-tests/tests/api/contracts.test.ts
+++ packages/integration-tests/tests/api/contracts.test.ts
@@ -1,35 +1,31 @@
 import * as request from "supertest";
+import { setTimeout } from "timers/promises";
 
 import { environment } from "../../src/config";
 import { localConfig } from "../../src/config";
 import { Buffer, Wallets } from "../../src/entities";
 import { Helper } from "../../src/helper";
-import { Playbook } from "../../src/playbook/playbook";
 
-describe("Contracts API", () => {
+describe("Contracts", () => {
   jest.setTimeout(localConfig.standardTimeout);
 
   const helper = new Helper();
-  const playbook = new Playbook();
   const bufferFile = "src/playbook/";
   let paymasterContract: string;
   let paymasterTx: string;
-  let multicallCallerContract: string;
-  let multicallCallerTx: string;
+
+  beforeAll(async () => {
+    paymasterContract = await helper.getStringFromFile(bufferFile + Buffer.paymaster);
+    paymasterTx = await helper.getStringFromFile(bufferFile + Buffer.paymasterDeployTx);
+  });
 
   describe("/api?module=contract&action=getcontractcreation", () => {
-    beforeAll(async () => {
-      await playbook.deployViaPaymaster();
-      await playbook.deployMultiCallContracts();
-    });
+    jest.setTimeout(localConfig.standardTimeout); //works unstable without timeout
 
     //@id1696
-    it("Verify the response via /api?module=contract&action=getcontractcreation&contractaddresses={address1},{address2}", async () => {
-      paymasterContract = await helper.getStringFromFile(bufferFile + Buffer.paymaster);
-      paymasterTx = await helper.getStringFromFile(bufferFile + Buffer.paymasterDeployTx);
-      multicallCallerContract = await helper.getStringFromFile(bufferFile + Buffer.addressMultiCallCaller);
-      multicallCallerTx = await helper.getStringFromFile(bufferFile + Buffer.txMultiCallCaller);
-      const apiRoute = `/api?module=contract&action=getcontractcreation&contractaddresses=${paymasterContract},${multicallCallerContract}`;
+    it("Verify the response via /api?module=contract&action=getcontractcreation", async () => {
+      await setTimeout(localConfig.extendedPause); //works unstable without timeout
+      const apiRoute = `/api?module=contract&action=getcontractcreation&contractaddresses=${paymasterContract}`;
       return request(environment.blockExplorerAPI)
         .get(apiRoute)
         .expect(200)
@@ -41,20 +37,7 @@ describe("Contracts API", () => {
             expect.objectContaining({ contractCreator: Wallets.richWalletAddress })
           )
         )
-        .expect((res) => expect(res.body.result[0]).toStrictEqual(expect.objectContaining({ txHash: paymasterTx })))
-        .expect((res) =>
-          expect(res.body.result[1]).toStrictEqual(
-            expect.objectContaining({ contractAddress: multicallCallerContract })
-          )
-        )
-        .expect((res) =>
-          expect(res.body.result[1]).toStrictEqual(
-            expect.objectContaining({ contractCreator: Wallets.richWalletAddress })
-          )
-        )
-        .expect((res) =>
-          expect(res.body.result[1]).toStrictEqual(expect.objectContaining({ txHash: multicallCallerTx }))
-        );
+        .expect((res) => expect(res.body.result[0]).toStrictEqual(expect.objectContaining({ txHash: paymasterTx })));
     });
   });
 });
