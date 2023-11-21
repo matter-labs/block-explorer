b3c73079ff2c59961bee03076aaf7a737b2d2083
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-11-21 13:24:10 +0200

    fix: token icon label

--- packages/app/tests/components/TokenIconLabel.spec.ts
+++ packages/app/tests/components/TokenIconLabel.spec.ts
@@ -6,7 +6,7 @@ import { describe, expect, it, vi } from "vitest";
 import { render } from "@testing-library/vue";
 import { mount, RouterLinkStub } from "@vue/test-utils";
 
-import { ETH_TOKEN_MOCK, useTokenLibraryMock } from "../mocks";
+import { ETH_TOKEN_MOCK } from "../mocks";
 
 import TokenIconLabel from "@/components/TokenIconLabel.vue";
 
@@ -29,31 +29,18 @@ describe("TokenIconLabel", () => {
     },
     plugins: [$testId, i18n],
   };
-  it("renders token icon for verified token", () => {
-    const mock = useTokenLibraryMock({
-      tokens: computed(() => []),
-      getTokens: vi.fn(),
-      getToken: () =>
-        <Api.Response.Token>{
-          ...ETH_TOKEN_MOCK,
-          iconURL: "https://test.link",
-        },
-    });
+  it("renders token icon if iconUrl is defined", () => {
     const wrapper = mount(TokenIconLabel, {
       global,
       props: {
         symbol: ETH_TOKEN_MOCK.symbol,
         address: ETH_TOKEN_MOCK.l2Address,
+        iconUrl: "https://test.link",
       },
     });
     expect(wrapper.find("img")?.attributes("src")).toBe("https://test.link");
-    mock.mockRestore();
   });
-  it("renders custom token icon for unverified token", () => {
-    const mock = useTokenLibraryMock({
-      tokens: computed(() => []),
-      getTokens: vi.fn(),
-    });
+  it("renders custom token icon if iconUrl is not defined", () => {
     const wrapper = mount(TokenIconLabel, {
       global,
       props: {
@@ -62,27 +49,8 @@ describe("TokenIconLabel", () => {
       },
     });
     expect(wrapper.find("img")?.attributes("src")).toBe("/images/currencies/customToken.svg");
-    mock.mockRestore();
-  });
-  it("hides image when tokens request is pending", () => {
-    const mock = useTokenLibraryMock({
-      isRequestPending: computed(() => true),
-      getTokens: vi.fn(),
-    });
-    const wrapper = mount(TokenIconLabel, {
-      global,
-      props: {
-        symbol: "ETH",
-        address: "0x0cc725e6ba24e7db79f62f22a7994a8ee33adc1b",
-      },
-    });
-    expect(wrapper.find(".token-img")?.classes().includes("loaded")).toBe(false);
-    mock.mockRestore();
   });
   it("renders token symbol when showLinkSymbol is true", () => {
-    const mock = useTokenLibraryMock({
-      getTokens: vi.fn(),
-    });
     const wrapper = mount(TokenIconLabel, {
       global,
       props: {
@@ -92,12 +60,8 @@ describe("TokenIconLabel", () => {
       },
     });
     expect(wrapper.find(".token-symbol").text()).toBe("ETH");
-    mock.mockRestore();
   });
   it("renders 'unknown' symbol when it is not exist", () => {
-    const mock = useTokenLibraryMock({
-      getTokens: vi.fn(),
-    });
     const wrapper = mount(TokenIconLabel, {
       global,
       props: {
@@ -107,27 +71,8 @@ describe("TokenIconLabel", () => {
       },
     });
     expect(wrapper.find(".token-symbol").text()).toBe(i18n.global.t("balances.table.unknownSymbol"));
-    mock.mockRestore();
-  });
-  it("renders custom token icon properly ", () => {
-    const mock = useTokenLibraryMock({
-      isTokenVerified: computed(() => false),
-      getTokens: vi.fn(),
-    });
-    const wrapper = mount(TokenIconLabel, {
-      global,
-      props: {
-        symbol: "LESA",
-        address: "0xc2675ae7f35b7d85ed1e828ccf6d0376b01adea3",
-      },
-    });
-    expect(wrapper.find("img")?.attributes("src")).toBe("/images/currencies/customToken.svg");
-    mock.mockRestore();
   });
   it("renders correct link of contract page", () => {
-    const mock = useTokenLibraryMock({
-      getTokens: vi.fn(),
-    });
     const wrapper = mount(TokenIconLabel, {
       global,
       props: {
@@ -139,12 +84,8 @@ describe("TokenIconLabel", () => {
 
     expect(contractPageUrl[0].props().to.name).toBe("address");
     expect(contractPageUrl[0].props().to.params.address).toBe("0xc2675AE7F35b7d85Ed1E828CCf6D0376B01ADea3");
-    mock.mockRestore();
   });
   it("renders default 'sm' size for icon", () => {
-    const mock = useTokenLibraryMock({
-      getTokens: vi.fn(),
-    });
     const { container } = render(TokenIconLabel, {
       global,
       props: {
@@ -154,12 +95,8 @@ describe("TokenIconLabel", () => {
     });
 
     expect(container.querySelector(".sm")).toBeTruthy();
-    mock.mockRestore();
   });
   it("renders 'md' size for icon", () => {
-    const mock = useTokenLibraryMock({
-      getTokens: vi.fn(),
-    });
     const { container } = render(TokenIconLabel, {
       global,
       props: {
@@ -170,12 +107,8 @@ describe("TokenIconLabel", () => {
     });
 
     expect(container.querySelector(".md")).toBeTruthy();
-    mock.mockRestore();
   });
   it("renders 'lg' size for icon", () => {
-    const mock = useTokenLibraryMock({
-      getTokens: vi.fn(),
-    });
     const { container } = render(TokenIconLabel, {
       global,
       props: {
@@ -186,6 +119,5 @@ describe("TokenIconLabel", () => {
     });
 
     expect(container.querySelector(".lg")).toBeTruthy();
-    mock.mockRestore();
   });
 });
