8f70d0d23f2a595982b1baebc854af8ce83fe436
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-12-04 13:10:00 +0200

    fix: different no transaction messages for non-existing and empty batches/blocks (#107)
    # What ❔
    
    Different _no transaction_ messages for non-existing and empty
    batches/blocks:
    Empty batch:
    <img width="1259" alt="image"
    src="https://github.com/matter-labs/block-explorer/assets/6553665/b3f8d9fc-dee8-4178-91c3-26eaff3be660">
    Non-existing batch:
    <img width="1263" alt="image"
    src="https://github.com/matter-labs/block-explorer/assets/6553665/da083f5f-c887-41f0-9173-0c51064fca91">
    
    ## Why ❔
    
    For better UX we want to show different _no transaction_ messages in
    different cases:
    `This Batch doesn’t have any transactions` - for empty batch,
    `This Batch has not been created or sealed yet` - for non-existing
    batch,
    same for blocks.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [X] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [X] Tests for the changes have been added / updated.

--- packages/app/tests/views/BatchView.spec.ts
+++ packages/app/tests/views/BatchView.spec.ts
@@ -3,16 +3,15 @@ import { createI18n } from "vue-i18n";
 import { describe, expect, it, vi } from "vitest";
 
 import { mount } from "@vue/test-utils";
-import { $fetch, FetchError } from "ohmyfetch";
 
 import enUS from "@/locales/en.json";
 
+import $testId from "@/plugins/testId";
 import routes from "@/router/routes";
 import BatchView from "@/views/BatchView.vue";
 
-const notFoundRoute = { name: "not-found", meta: { title: "404 Not Found" } };
 const router = {
-  resolve: vi.fn(() => notFoundRoute),
+  resolve: vi.fn(),
   replace: vi.fn(),
   currentRoute: {
     value: {},
@@ -54,27 +53,6 @@ describe("BatchView:", () => {
     expect(i18n.global.t(routes.find((e) => e.name === "batch")?.meta?.title as string)).toBe("Batch");
   });
 
-  it("route is replaced with not found view on request 404 error", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const error: any = new FetchError("404");
-    error.response = {
-      status: 404,
-    };
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const mock = ($fetch as any).mockRejectedValue(error);
-    mount(BatchView, {
-      props: {
-        id: "42",
-      },
-      global: {
-        stubs: ["router-link"],
-        plugins: [i18n],
-      },
-    });
-    await new Promise((resolve) => setImmediate(resolve));
-    expect(router.replace).toHaveBeenCalledWith(notFoundRoute);
-    mock.mockRestore();
-  });
   it("shows correct trimmed title", () => {
     const wrapper = mount(BatchView, {
       props: {
@@ -82,9 +60,10 @@ describe("BatchView:", () => {
       },
       global: {
         stubs: ["router-link"],
-        plugins: [i18n],
+        plugins: [i18n, $testId],
       },
     });
+
     expect(wrapper.find(".breadcrumb-item span").text()).toBe("Batch #42");
   });
 });
