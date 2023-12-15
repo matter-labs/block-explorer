0aeb85f098bf7884b0f003915d6fefe32c07a8ad
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-12-15 14:06:26 +0200

    feat: deprecate warning for goerli (#126)
    # What ❔
    
    Deprecate warning for Goerli network:
    <img width="1279" alt="image"
    src="https://github.com/matter-labs/block-explorer/assets/6553665/6f1e0117-3561-4d85-a2a4-dd8fc53b695a">
    
    
    ## Why ❔
    
    To warn users that we are ending support of Goerli testnet.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [X] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -3,7 +3,6 @@
     <the-header :class="$route?.name" />
     <div class="container-app">
       <NetworkDeprecated v-if="!currentNetwork.maintenance && currentNetwork.name === 'goerli'" />
-      <IndexerDelayAlert v-if="!currentNetwork.maintenance && currentNetwork.name === 'mainnet'" />
       <MaintenanceView v-if="currentNetwork.maintenance" />
       <router-view v-else />
     </div>
@@ -14,7 +13,6 @@
 <script setup lang="ts">
 import { useTitle } from "@vueuse/core";
 
-import IndexerDelayAlert from "@/components/IndexerDelayAlert.vue";
 import NetworkDeprecated from "@/components/NetworkDeprecated.vue";
 import TheFooter from "@/components/TheFooter.vue";
 import TheHeader from "@/components/header/TheHeader.vue";
