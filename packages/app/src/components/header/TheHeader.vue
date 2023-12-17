293148797423487663e3d94b3409e036f3454c73
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2023-12-17 08:16:04 +0200

    fix: remove mainnet warning (#133)
    # What ❔
    
    Remove warning message for mainnet since we don't need it anymore.

--- packages/app/src/components/header/TheHeader.vue
+++ packages/app/src/components/header/TheHeader.vue
@@ -301,10 +301,6 @@ const hasContent = computed(() => {
   .hero-banner-container {
     @apply absolute left-0 top-full flex h-64 w-full items-end justify-end overflow-hidden bg-primary-900;
 
-    &.mainnet {
-      @apply h-[27rem] md:h-[23rem] lg:h-[20rem];
-    }
-
     &.goerli {
       @apply h-[25rem] md:h-[23rem] lg:h-[19rem];
     }
@@ -315,10 +311,6 @@ const hasContent = computed(() => {
   }
   .home-banner {
     @apply h-80;
-
-    &.mainnet {
-      @apply h-[32rem] md:h-[28rem] lg:h-[25rem];
-    }
 
     &.goerli {
       @apply h-[30rem] md:h-[27rem] lg:h-[24rem];
