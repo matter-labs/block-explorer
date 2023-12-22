a4f04eef1bb0d34f7fb68b9fd87d7ffefdbdb47a
Roman Petriv <petriv.roma@gmail.com>
2023-12-22 18:00:21 +0200

    feat: automatic system alerts (#141)

--- packages/worker/src/health/health.controller.ts
+++ packages/worker/src/health/health.controller.ts
@@ -1,5 +1,5 @@
 import { Logger, Controller, Get } from "@nestjs/common";
-import { HealthCheckService, /*TypeOrmHealthIndicator,*/ HealthCheck, HealthCheckResult } from "@nestjs/terminus";
+import { HealthCheckService, TypeOrmHealthIndicator, HealthCheck, HealthCheckResult } from "@nestjs/terminus";
 import { JsonRpcHealthIndicator } from "./jsonRpcProvider.health";
 
 @Controller(["health", "ready"])
@@ -8,7 +8,7 @@ export class HealthController {
 
   constructor(
     private readonly healthCheckService: HealthCheckService,
-    //private readonly dbHealthChecker: TypeOrmHealthIndicator,
+    private readonly dbHealthChecker: TypeOrmHealthIndicator,
     private readonly jsonRpcHealthIndicator: JsonRpcHealthIndicator
   ) {
     this.logger = new Logger(HealthController.name);
@@ -19,7 +19,7 @@ export class HealthController {
   public async check(): Promise<HealthCheckResult> {
     try {
       return await this.healthCheckService.check([
-        //() => this.dbHealthChecker.pingCheck("database"),
+        () => this.dbHealthChecker.pingCheck("database"),
         () => this.jsonRpcHealthIndicator.isHealthy("jsonRpcProvider"),
       ]);
     } catch (error) {
