6a60119b4505a83eebbc494339fa164ac70879a7
Roman Petriv <petriv.roma@gmail.com>
2023-12-22 07:09:22 +0200

    feat: add automatic system alert banner

--- packages/app/src/components/NetworkDeprecated.vue
+++ packages/app/src/components/NetworkDeprecated.vue
@@ -1,25 +1,19 @@
 <template v-if="newNetworkUrl">
-  <div class="deprecated-alert">
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M21.0907 18.7005C20.8457 19.1905 20.3449 19.5 19.7971 19.5H4.20288C3.65506 19.5 3.15426 19.1905 2.90927 18.7005C2.69455 18.2711 2.70694 17.7631 2.94232 17.3446L10.9455 3.11671C11.1598 2.73575 11.5629 2.5 12 2.5C12.4371 2.5 12.8402 2.73575 13.0545 3.11671L21.0577 17.3446C21.2931 17.7631 21.3054 18.2711 21.0907 18.7005Z"
-        stroke="#FFC81A"
-      />
-      <circle cx="12" cy="16" r="1" fill="#FFC81A" />
-      <path d="M12 14V6" stroke="#FFC81A" />
-    </svg>
+  <SystemAlert>
     <span
       >We are ending our support of Goerli testnet. Please <a :href="newNetworkUrl!">use Sepolia</a>. For more info see
       <a target="_blank" href="https://github.com/zkSync-Community-Hub/zkync-developers/discussions/228"
         >this announcement</a
       >.</span
     >
-  </div>
+  </SystemAlert>
 </template>
 
 <script lang="ts" setup>
 import { computed } from "vue";
 import { useRoute } from "vue-router";
+
+import SystemAlert from "@/components/common/SystemAlert.vue";
 
 import useContext from "@/composables/useContext";
 
@@ -44,15 +38,7 @@ const newNetworkUrl = computed(() => {
 </script>
 
 <style scoped lang="scss">
-.deprecated-alert {
-  @apply flex text-white rounded-2xl border border-amber-400/50 bg-amber-400/10 mb-6 py-3 px-4;
-
-  svg {
-    @apply mr-2 shrink-0;
-  }
-
-  a {
-    @apply text-inherit;
-  }
+a {
+  @apply text-inherit;
 }
 </style>
--- packages/worker/src/main.ts
+++ packages/worker/src/main.ts
@@ -3,54 +3,11 @@ import { ConfigService } from "@nestjs/config";
 import logger from "./logger";
 import { AppModule } from "./app.module";
 
-import { initializeApp } from "firebase/app";
-import { getFirestore, onSnapshot, doc } from "firebase/firestore";
-
-const firebaseApp = initializeApp({
-  projectId: "scan-v2",
-});
-const db = getFirestore(firebaseApp);
-
-const getConfig = async (instanceId: string): Promise<{ fromBlock: string; toBlock: string }> => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = onSnapshot(
-      doc(db, "config", "worker"),
-      (data) => {
-        if (data.exists()) {
-          const config = data.data() as Record<string, { fromBlock: string; toBlock: string }>;
-          if (config[instanceId] && config[instanceId].fromBlock && config[instanceId].toBlock) {
-            unsubscribe();
-            resolve(config[instanceId]);
-          }
-        }
-      },
-      (e) => {
-        logger.error(e);
-        reject(e);
-      }
-    );
-  });
-};
-
 async function bootstrap() {
   process.on("uncaughtException", function (error) {
     logger.error(error.message, error.stack, "UnhandledExceptions");
     process.exit(1);
   });
-
-  if (process.env.USE_REMOTE_CONFIG === "true") {
-    const config = await getConfig(process.env.HOSTNAME);
-    if (!config.fromBlock || !config.toBlock) {
-      throw new Error("Missing fromBlock or toBlock in config");
-    }
-    logger.log(`Using fromBlock: ${config.fromBlock}, toBlock: ${config.toBlock}`);
-    process.env.FROM_BLOCK = config.fromBlock;
-    process.env.TO_BLOCK = config.toBlock;
-    process.env.DISABLE_BATCHES_PROCESSING = "true";
-    process.env.DISABLE_COUNTERS_PROCESSING = "true";
-    process.env.DISABLE_OLD_BALANCES_CLEANER = "true";
-    process.env.DISABLE_BLOCKS_REVERT = "true";
-  }
 
   const app = await NestFactory.create(AppModule, {
     logger,
