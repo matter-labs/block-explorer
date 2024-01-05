ac1c2658a53e71c9be286167a276f7e6fd6cd9ea
pcheremu <pavel.cheremushkin@gmail.com>
2024-01-05 11:56:53 +0100

    test: initial commit with tests

--- packages/integration-tests/src/helper.ts
+++ packages/integration-tests/src/helper.ts
@@ -1,7 +1,10 @@
 import { execSync } from "child_process";
+import { ethers } from "ethers";
 import { promises as fs } from "fs";
+import { Provider } from "zksync-web3";
 import * as path from "path";
 
+import { localConfig } from "./config";
 import { Logger } from "./entities";
 
 export class Helper {
@@ -34,5 +37,22 @@ export class Helper {
     } catch {
       console.log(`There is no the expected file: ${fileName}`);
     }
+  }
+
+  async getBalanceETH(walletAddress: string, layer: string) {
+    let network: string;
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+    let provider: any;
+    if (layer == "L1") {
+      network = localConfig.L1Network;
+      provider = ethers.getDefaultProvider(network);
+    } else if (layer == "L2") {
+      network = localConfig.L2Network;
+      provider = new Provider(network);
+    } else {
+      console.log(`Wrong layer: ${layer}`);
+    }
+    const balanceEth = ethers.utils.formatUnits(await provider.getBalance(walletAddress), "wei");
+    return balanceEth;
   }
 }
---
+++ packages/integration-tests/tests/api/accounts.test.ts
@@ -0,0 +1,123 @@
+import * as request from "supertest";
+import { setTimeout } from "timers/promises";
+
+import { environment } from "../../src/config";
+import { localConfig } from "../../src/config";
+import { Token, Wallets } from "../../src/entities";
+
+describe("Accounts API", () => {
+  //@id1704
+  it(
+    "Verify /api?module=account&action=balancemulti response returns elements" +
+    " balancemulti => balancemulti&address={account_address1},{account_address2}",
+    async () => {
+      const apiRoute = `/api?module=account&action=balancemulti&address=${Wallets.richWalletAddress},${Wallets.mainWalletAddress}`;
+      const richWalletBalance = await helper.getBalanceETH(Wallets.richWalletAddress, "L2");
+      const mainWalletBalance = await helper.getBalanceETH(Wallets.mainWalletAddress, "L2");
+      const richWalletLowerCase = Wallets.richWalletAddress.toLowerCase();
+      const mainWalletLowerCase = Wallets.mainWalletAddress.toLowerCase();
+      await setTimeout(localConfig.extendedPause); //works unstable without timeout
+
+      return request(environment.blockExplorerAPI)
+        .get(apiRoute)
+        .expect(200)
+        .expect((res) => expect(res.body.result.length).toBeGreaterThan(1))
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ status: "1" })))
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ message: "OK" })))
+        .expect((res) =>
+          expect(res.body.result[0]).toStrictEqual(
+            expect.objectContaining({ account: richWalletLowerCase, balance: richWalletBalance })
+          )
+        )
+        .expect((res) =>
+          expect(res.body.result[1]).toStrictEqual(
+            expect.objectContaining({ account: mainWalletLowerCase, balance: mainWalletBalance })
+          )
+        );
+    }
+  );
+
+  //@id1703
+  it(
+    "Verify /api?module=account&action=balance response returns elements" +
+    "balance => balance&address={account_address}",
+    async () => {
+      const apiRoute = `/api?module=account&action=balance&address=${Wallets.richWalletAddress}`;
+      const balance = await helper.getBalanceETH(Wallets.richWalletAddress, "L2");
+      await setTimeout(localConfig.extendedPause); //works unstable without timeout
+
+      return request(environment.blockExplorerAPI)
+        .get(apiRoute)
+        .expect(200)
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ status: "1" })))
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ message: "OK" })))
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ result: balance })));
+    }
+  );
+
+  //@id1705
+  it(
+    "Verify /api?module=account&action=tokenbalance response returns elements" +
+    " tokenbalance => tokenbalance&contractaddress={contract_address}&address={account_address}",
+    async () => {
+      const apiRoute = `/api?module=account&action=tokenbalance&contractaddress=${Token.ETHER_ERC20_Address}&address=${Wallets.richWalletAddress}`;
+      await setTimeout(localConfig.extendedPause); //works unstable without timeout
+
+      return request(environment.blockExplorerAPI)
+        .get(apiRoute)
+        .expect(200)
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ status: "1" })))
+        .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ message: "OK" })))
+        .expect((res) => expect(typeof res.body.result).toStrictEqual("string"));
+    }
+  );
+
+  //@id1702
+  it(
+    "Verify /api?module=account&action=txlist response returns elements" +
+    " txlist => txlist&page=1&offset=10&sort=desc&endblock{block_number}&startblock=0&address={account_address}",
+    async () => {
+      const blocks = await request(environment.blockExplorerAPI).get("/blocks");
+
+      const blockNumber = blocks.body.items[0].number;
+      const apiRoute = `/api?module=account&action=txlist&page=1&offset=10&sort=desc&endblock${blockNumber}&startblock=0&address=${Wallets.richWalletAddress}`;
+
+      await setTimeout(localConfig.extendedPause); //works unstable without timeout
+
+      return (
+        request(environment.blockExplorerAPI)
+          .get(apiRoute)
+          .expect(200)
+          .expect((res) => expect(res.body.result.length).toBeGreaterThan(1))
+          .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ status: "1" })))
+          .expect((res) => expect(res.body).toStrictEqual(expect.objectContaining({ message: "OK" })))
+          .expect((res) => expect(typeof res.body.result[0].blockNumber).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].timeStamp).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].hash).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].nonce).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].blockHash).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].transactionIndex).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].from).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].to).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].value).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].gas).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].gasPrice).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].isError).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].txreceipt_status).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].input).toStrictEqual("string"))
+          // .expect((res) => expect(typeof res.body.result[0].contractAddress).toStrictEqual("string")) // can be null
+          .expect((res) => expect(typeof res.body.result[0].cumulativeGasUsed).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].gasUsed).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].confirmations).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].fee).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].commitTxHash).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].proveTxHash).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].executeTxHash).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].isL1Originated).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].l1BatchNumber).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].methodId).toStrictEqual("string"))
+          .expect((res) => expect(typeof res.body.result[0].functionName).toStrictEqual("string"))
+      );
+    }
+  );
+});
