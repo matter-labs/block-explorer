596347c1025b60f33f91de14d7a49ca6c0c9559f
abilevych <anton@matterlabs.dev>
2024-01-11 15:58:59 +0200

    chore: api retry logic for tests

--- packages/integration-tests/src/config.ts
+++ packages/integration-tests/src/config.ts
@@ -1,6 +1,7 @@
 import { Wallets } from "./entities";
 
 export const localConfig = {
+  debugAPIwrapper: false,
   gasLimit: { gasLimit: 10000000 },
   l2GasLimit: 10000000,
   L1Network: "http://localhost:8545",
@@ -11,6 +12,8 @@ export const localConfig = {
   extendedPause: 20 * 1000,
   standardPause: 5 * 1000,
   minimalPause: 1 * 1000,
+  maxAPIretries: 10,
+  intervalAPIretries: 0.5 * 1000,
 };
 
 export const environment = {
