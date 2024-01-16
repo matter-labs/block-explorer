c50eb52cd416aa43b41ff13135f73fa77f2f1cec
pcheremu <pavel.cheremushkin@gmail.com>
2024-01-16 11:47:28 +0100

    test: fix eslint

--- packages/integration-tests/tests/api/logs.test.ts
+++ packages/integration-tests/tests/api/logs.test.ts
@@ -7,7 +7,7 @@ import { Buffer } from "../../src/entities";
 import { Helper } from "../../src/helper";
 import { Playbook } from "../../src/playbook/playbook";
 
-xdescribe("/api?module=logs", () => {
+describe("/api?module=logs", () => {
   jest.setTimeout(localConfig.standardTimeout); //works unstable without timeout
   const helper = new Helper();
   const bufferFile = "src/playbook/";
