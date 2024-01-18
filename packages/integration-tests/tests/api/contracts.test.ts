63632ed3ba8f5f4e92e5a28a513e7537e57c7ba3
pcheremu <pavel.cheremushkin@gmail.com>
2024-01-18 14:06:48 +0100

    test: fix names in tests

--- packages/integration-tests/tests/api/contracts.test.ts
+++ packages/integration-tests/tests/api/contracts.test.ts
@@ -7,7 +7,7 @@ import { Buffer, Wallets } from "../../src/entities";
 import { Helper } from "../../src/helper";
 import { Playbook } from "../../src/playbook/playbook";
 
-describe("/api?module=contract", () => {
+describe("API module: Contract", () => {
   jest.setTimeout(localConfig.standardTimeout);
 
   const helper = new Helper();
