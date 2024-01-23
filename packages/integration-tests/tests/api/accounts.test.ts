ee12d94e8055925ca2089281b50fe216f7dfde63
abilevych <anton@matterlabs.dev>
2024-01-23 12:54:15 +0200

    chore: updated retry-logic for integration api tests

--- packages/integration-tests/tests/api/accounts.test.ts
+++ packages/integration-tests/tests/api/accounts.test.ts
@@ -14,84 +14,92 @@ describe("API module: Account", () => {
 
   //@id1704
   it("Verify /api?module=account&action=balancemulti response", async () => {
-    apiRoute = `/api?module=account&action=balancemulti&address=${Wallets.richWalletAddress},${Wallets.mainWalletAddress}`;
-    const richWalletBalance = await helper.getBalanceETH(Wallets.richWalletAddress, "L2");
-    const mainWalletBalance = await helper.getBalanceETH(Wallets.mainWalletAddress, "L2");
-    const richWalletLowerCase = Wallets.richWalletAddress.toLowerCase();
-    const mainWalletLowerCase = Wallets.mainWalletAddress.toLowerCase();
-    response = await helper.retryAPIrequest(apiRoute);
+    await helper.retryTestAction(async () => {
+      apiRoute = `/api?module=account&action=balancemulti&address=${Wallets.richWalletAddress},${Wallets.mainWalletAddress}`;
+      const richWalletBalance = await helper.getBalanceETH(Wallets.richWalletAddress, "L2");
+      const mainWalletBalance = await helper.getBalanceETH(Wallets.mainWalletAddress, "L2");
+      const richWalletLowerCase = Wallets.richWalletAddress.toLowerCase();
+      const mainWalletLowerCase = Wallets.mainWalletAddress.toLowerCase();
+      response = await helper.performGETrequest(apiRoute);
 
-    expect(response.status).toBe(200);
-    expect(response.body.result.length).toBeGreaterThan(1);
-    expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-    expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-    expect(response.body.result[0]).toStrictEqual(
-      expect.objectContaining({ account: richWalletLowerCase, balance: richWalletBalance })
-    );
-    expect(response.body.result[1]).toStrictEqual(
-      expect.objectContaining({ account: mainWalletLowerCase, balance: mainWalletBalance })
-    );
+      expect(response.status).toBe(200);
+      expect(response.body.result.length).toBeGreaterThan(1);
+      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+      expect(response.body.result[0]).toStrictEqual(
+        expect.objectContaining({ account: richWalletLowerCase, balance: richWalletBalance })
+      );
+      expect(response.body.result[1]).toStrictEqual(
+        expect.objectContaining({ account: mainWalletLowerCase, balance: mainWalletBalance })
+      );
+    });
   });
 
   //@id1703
   it("Verify /api?module=account&action=balance response", async () => {
-    const balance = await helper.getBalanceETH(Wallets.richWalletAddress, "L2");
-    apiRoute = `/api?module=account&action=balance&address=${Wallets.richWalletAddress}`;
-    response = await helper.retryAPIrequest(apiRoute);
+    await helper.retryTestAction(async () => {
+      const balance = await helper.getBalanceETH(Wallets.richWalletAddress, "L2");
+      apiRoute = `/api?module=account&action=balance&address=${Wallets.richWalletAddress}`;
+      response = await helper.performGETrequest(apiRoute);
 
-    expect(response.status).toBe(200);
-    expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-    expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-    expect(response.body).toStrictEqual(expect.objectContaining({ result: balance }));
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+      expect(response.body).toStrictEqual(expect.objectContaining({ result: balance }));
+    });
   });
 
   //@id1705
   it("Verify /api?module=account&action=tokenbalance response", async () => {
-    apiRoute = `/api?module=account&action=tokenbalance&contractaddress=${Token.ETHER_ERC20_Address}&address=${Wallets.richWalletAddress}`;
-    response = await helper.retryAPIrequest(apiRoute);
+    await helper.retryTestAction(async () => {
+      apiRoute = `/api?module=account&action=tokenbalance&contractaddress=${Token.ETHER_ERC20_Address}&address=${Wallets.richWalletAddress}`;
+      response = await helper.performGETrequest(apiRoute);
 
-    expect(response.status).toBe(200);
-    expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-    expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-    expect(typeof response.body.result).toStrictEqual("string");
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+      expect(typeof response.body.result).toStrictEqual("string");
+    });
   });
 
   //@id1702
-  xit("Verify /api?module=account&action=txlist response", async () => {
-    const blocks = await request(environment.blockExplorerAPI).get("/blocks");
-    const blockNumber = blocks.body.items[0].number;
-    apiRoute = `/api?module=account&action=txlist&page=1&offset=10&sort=desc&endblock${blockNumber}&startblock=0&address=${Wallets.richWalletAddress}`;
-    response = await helper.retryAPIrequest(apiRoute);
+  it("Verify /api?module=account&action=txlist response", async () => {
+    await helper.retryTestAction(async () => {
+      const blocks = await request(environment.blockExplorerAPI).get("/blocks");
+      const blockNumber = blocks.body.items[0].number;
+      apiRoute = `/api?module=account&action=txlist&page=1&offset=10&sort=desc&endblock${blockNumber}&startblock=0&address=${Wallets.richWalletAddress}`;
+      response = await helper.performGETrequest(apiRoute);
 
-    expect(response.status).toBe(200);
-    expect(response.body.result.length).toBeGreaterThan(1);
-    expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-    expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-    expect(typeof response.body.result[0].blockNumber).toStrictEqual("string");
-    expect(typeof response.body.result[0].timeStamp).toStrictEqual("string");
-    expect(typeof response.body.result[0].hash).toStrictEqual("string");
-    expect(typeof response.body.result[0].nonce).toStrictEqual("string");
-    expect(typeof response.body.result[0].blockHash).toStrictEqual("string");
-    expect(typeof response.body.result[0].transactionIndex).toStrictEqual("string");
-    expect(typeof response.body.result[0].from).toStrictEqual("string");
-    expect(typeof response.body.result[0].to).toStrictEqual("string");
-    expect(typeof response.body.result[0].value).toStrictEqual("string");
-    expect(typeof response.body.result[0].gas).toStrictEqual("string");
-    expect(typeof response.body.result[0].gasPrice).toStrictEqual("string");
-    expect(typeof response.body.result[0].isError).toStrictEqual("string");
-    expect(typeof response.body.result[0].txreceipt_status).toStrictEqual("string");
-    expect(typeof response.body.result[0].input).toStrictEqual("string");
-    expect(typeof response.body.result[0].contractAddress).toBeTruthy(); // can be null
-    expect(typeof response.body.result[0].cumulativeGasUsed).toStrictEqual("string");
-    expect(typeof response.body.result[0].gasUsed).toStrictEqual("string");
-    expect(typeof response.body.result[0].confirmations).toStrictEqual("string");
-    expect(typeof response.body.result[0].fee).toStrictEqual("string");
-    expect(typeof response.body.result[0].commitTxHash).toStrictEqual("string");
-    expect(typeof response.body.result[0].proveTxHash).toStrictEqual("string");
-    expect(typeof response.body.result[0].executeTxHash).toStrictEqual("string");
-    expect(typeof response.body.result[0].isL1Originated).toStrictEqual("string");
-    expect(typeof response.body.result[0].l1BatchNumber).toStrictEqual("string");
-    expect(typeof response.body.result[0].methodId).toStrictEqual("string");
-    expect(typeof response.body.result[0].functionName).toStrictEqual("string");
+      expect(response.status).toBe(200);
+      expect(response.body.result.length).toBeGreaterThan(1);
+      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+      expect(typeof response.body.result[0].blockNumber).toStrictEqual("string");
+      expect(typeof response.body.result[0].timeStamp).toStrictEqual("string");
+      expect(typeof response.body.result[0].hash).toStrictEqual("string");
+      expect(typeof response.body.result[0].nonce).toStrictEqual("string");
+      expect(typeof response.body.result[0].blockHash).toStrictEqual("string");
+      expect(typeof response.body.result[0].transactionIndex).toStrictEqual("string");
+      expect(typeof response.body.result[0].from).toStrictEqual("string");
+      expect(typeof response.body.result[0].to).toStrictEqual("string");
+      expect(typeof response.body.result[0].value).toStrictEqual("string");
+      expect(typeof response.body.result[0].gas).toStrictEqual("string");
+      expect(typeof response.body.result[0].gasPrice).toStrictEqual("string");
+      expect(typeof response.body.result[0].isError).toStrictEqual("string");
+      expect(typeof response.body.result[0].txreceipt_status).toStrictEqual("string");
+      expect(typeof response.body.result[0].input).toStrictEqual("string");
+      expect(typeof response.body.result[0].contractAddress).toBeTruthy(); // can be null
+      expect(typeof response.body.result[0].cumulativeGasUsed).toStrictEqual("string");
+      expect(typeof response.body.result[0].gasUsed).toStrictEqual("string");
+      expect(typeof response.body.result[0].confirmations).toStrictEqual("string");
+      expect(typeof response.body.result[0].fee).toStrictEqual("string");
+      expect(typeof response.body.result[0].commitTxHash).toStrictEqual("string");
+      expect(typeof response.body.result[0].proveTxHash).toStrictEqual("string");
+      expect(typeof response.body.result[0].executeTxHash).toStrictEqual("string");
+      expect(typeof response.body.result[0].isL1Originated).toStrictEqual("string");
+      expect(typeof response.body.result[0].l1BatchNumber).toStrictEqual("string");
+      expect(typeof response.body.result[0].methodId).toStrictEqual("string");
+      expect(typeof response.body.result[0].functionName).toStrictEqual("string");
+    });
   });
 });
--- packages/integration-tests/tests/api/blocks.test.ts
+++ packages/integration-tests/tests/api/blocks.test.ts
@@ -10,98 +10,108 @@ describe("Blocks", () => {
     jest.setTimeout(localConfig.standardTimeout);
     //@id1511
     it("Verify the response via /blocks", async () => {
-      apiRoute = `/blocks`;
-      response = await helper.retryAPIrequest(apiRoute);
+      await helper.retryTestAction(async () => {
+        apiRoute = `/blocks`;
+        response = await helper.performGETrequest(apiRoute);
 
-      expect(response.status).toBe(200);
-      expect(Array.isArray(response.body.items)).toStrictEqual(true);
-      expect(response.body.items.length).toBeGreaterThan(1);
-      expect(typeof response.body.meta.totalItems).toStrictEqual("number");
-      expect(typeof response.body.meta.itemCount).toStrictEqual("number");
-      expect(typeof response.body.meta.itemsPerPage).toStrictEqual("number");
-      expect(typeof response.body.meta.totalPages).toStrictEqual("number");
-      expect(typeof response.body.meta.currentPage).toStrictEqual("number");
-      expect(typeof response.body.links.first).toStrictEqual("string");
-      expect(typeof response.body.links.previous).toStrictEqual("string");
-      expect(typeof response.body.links.next).toStrictEqual("string");
-      expect(typeof response.body.links.last).toStrictEqual("string");
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body.items)).toStrictEqual(true);
+        expect(response.body.items.length).toBeGreaterThan(1);
+        expect(typeof response.body.meta.totalItems).toStrictEqual("number");
+        expect(typeof response.body.meta.itemCount).toStrictEqual("number");
+        expect(typeof response.body.meta.itemsPerPage).toStrictEqual("number");
+        expect(typeof response.body.meta.totalPages).toStrictEqual("number");
+        expect(typeof response.body.meta.currentPage).toStrictEqual("number");
+        expect(typeof response.body.links.first).toStrictEqual("string");
+        expect(typeof response.body.links.previous).toStrictEqual("string");
+        expect(typeof response.body.links.next).toStrictEqual("string");
+        expect(typeof response.body.links.last).toStrictEqual("string");
+      });
     });
 
     //@id1512 //unstable on CI
-    xit("Verify the response via /blocks/{/blockNumber}", async () => {
-      const blocks = await await helper.retryAPIrequest("/blocks");
-      const blockNumber = blocks.body.items[0].number;
-      apiRoute = `/blocks/${blockNumber}`;
-      response = await helper.retryAPIrequest(apiRoute);
+    it("Verify the response via /blocks/{/blockNumber}", async () => {
+      await helper.retryTestAction(async () => {
+        const blocks = await helper.performGETrequest("/blocks");
+        const blockNumber = blocks.body.items[0].number;
+        apiRoute = `/blocks/${blockNumber}`;
+        response = await helper.performGETrequest(apiRoute);
 
-      expect(response.status).toBe(200);
-      expect(response.body.number).toStrictEqual(blockNumber);
-      expect(typeof response.body.hash).toStrictEqual("string");
-      expect(typeof response.body.timestamp).toStrictEqual("string");
-      expect(typeof response.body.gasUsed).toStrictEqual("string");
-      expect(typeof response.body.l1BatchNumber).toStrictEqual("number");
-      expect(typeof response.body.l1TxCount).toStrictEqual("number");
-      expect(typeof response.body.l2TxCount).toStrictEqual("number");
-      expect(typeof response.body.parentHash).toStrictEqual("string");
-      expect(typeof response.body.gasLimit).toStrictEqual("string");
-      expect(typeof response.body.baseFeePerGas).toStrictEqual("string");
-      expect(typeof response.body.extraData).toStrictEqual("string");
-      expect(typeof response.body.size).toStrictEqual("number");
-      expect(typeof response.body.status).toStrictEqual("string");
-      expect(typeof response.body.isL1BatchSealed).toStrictEqual("boolean");
-      expect(typeof response.body.commitTxHash).toStrictEqual("string");
-      // .expect((res) => expect(typeof res.body.commitTxHash).toStrictEqual("string")) //unstable on a CI
-      expect(typeof response.body.proveTxHash).toStrictEqual("string");
-      expect(typeof response.body.committedAt).toStrictEqual("string");
-      expect(typeof response.body.executedAt).toStrictEqual("string");
-      expect(typeof response.body.provenAt).toStrictEqual("string");
+        expect(response.status).toBe(200);
+        expect(response.body.number).toStrictEqual(blockNumber);
+        expect(typeof response.body.hash).toStrictEqual("string");
+        expect(typeof response.body.timestamp).toStrictEqual("string");
+        expect(typeof response.body.gasUsed).toStrictEqual("string");
+        expect(typeof response.body.l1BatchNumber).toStrictEqual("number");
+        expect(typeof response.body.l1TxCount).toStrictEqual("number");
+        expect(typeof response.body.l2TxCount).toStrictEqual("number");
+        expect(typeof response.body.parentHash).toStrictEqual("string");
+        expect(typeof response.body.gasLimit).toStrictEqual("string");
+        expect(typeof response.body.baseFeePerGas).toStrictEqual("string");
+        expect(typeof response.body.extraData).toStrictEqual("string");
+        expect(typeof response.body.size).toStrictEqual("number");
+        expect(typeof response.body.status).toStrictEqual("string");
+        expect(typeof response.body.isL1BatchSealed).toStrictEqual("boolean");
+        expect(typeof response.body.commitTxHash).toStrictEqual("string");
+        expect((res) => expect(typeof res.body.commitTxHash).toStrictEqual("string"));
+        expect(typeof response.body.proveTxHash).toStrictEqual("string");
+        expect(typeof response.body.committedAt).toStrictEqual("string");
+        expect(typeof response.body.executedAt).toStrictEqual("string");
+        expect(typeof response.body.provenAt).toStrictEqual("string");
+      });
     });
   });
 
   describe("/api?module=block", () => {
     //@id1700
     it("Verify /api?module=block&action=getblockcountdown&blockno={block_number} response", async () => {
-      const blocks = await helper.retryAPIrequest("/blocks");
-      const blockNumber = blocks.body.items[0].number + 1;
-      apiRoute = `/api?module=block&action=getblockcountdown&blockno=${blockNumber}`;
-      response = await helper.retryAPIrequest(apiRoute);
+      await helper.retryTestAction(async () => {
+        const blocks = await helper.performGETrequest("/blocks");
+        const blockNumber = blocks.body.items[0].number + 1;
+        apiRoute = `/api?module=block&action=getblockcountdown&blockno=${blockNumber}`;
+        response = await helper.performGETrequest(apiRoute);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-      expect(typeof response.body.result.CurrentBlock).toStrictEqual("string");
-      expect(typeof response.body.result.CountdownBlock).toStrictEqual("string");
-      expect(typeof response.body.result.RemainingBlock).toStrictEqual("string");
-      expect(typeof response.body.result.EstimateTimeInSec).toStrictEqual("string");
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+        expect(typeof response.body.result.CurrentBlock).toStrictEqual("string");
+        expect(typeof response.body.result.CountdownBlock).toStrictEqual("string");
+        expect(typeof response.body.result.RemainingBlock).toStrictEqual("string");
+        expect(typeof response.body.result.EstimateTimeInSec).toStrictEqual("string");
+      });
     });
 
     //@id1699
     it("Verify /api?module=block&action=getblocknobytime&closest=before&timestamp={timestamp} response", async () => {
-      apiRoute = `/api?module=block&action=getblocknobytime&closest=before&timestamp=1635934550`;
-      response = await helper.retryAPIrequest(apiRoute);
+      await helper.retryTestAction(async () => {
+        apiRoute = `/api?module=block&action=getblocknobytime&closest=before&timestamp=1635934550`;
+        response = await helper.performGETrequest(apiRoute);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-      expect(typeof response.body.result).toStrictEqual("string");
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+        expect(typeof response.body.result).toStrictEqual("string");
+      });
     });
 
     //@id1701
     it("Verify /api?module=block&action=getblockreward&blockno={blockNumber} response", async () => {
-      const blocks = await helper.retryAPIrequest("/blocks");
-      const blockNumber = blocks.body.items[0].number;
-      apiRoute = `/api?module=block&action=getblockreward&blockno=${blockNumber}`;
-      response = await helper.retryAPIrequest(apiRoute);
+      await helper.retryTestAction(async () => {
+        const blocks = await helper.performGETrequest("/blocks");
+        const blockNumber = blocks.body.items[0].number;
+        apiRoute = `/api?module=block&action=getblockreward&blockno=${blockNumber}`;
+        response = await helper.performGETrequest(apiRoute);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
-      expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
-      expect(typeof response.body.result.blockNumber).toStrictEqual("string");
-      expect(typeof response.body.result.timeStamp).toStrictEqual("string");
-      expect(typeof response.body.result.blockMiner).toStrictEqual("string");
-      expect(typeof response.body.result.blockReward).toStrictEqual("string");
-      expect(typeof response.body.result.uncleInclusionReward).toStrictEqual("string");
-      expect(typeof response.body.result.uncles).toStrictEqual("object");
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "1" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "OK" }));
+        expect(typeof response.body.result.blockNumber).toStrictEqual("string");
+        expect(typeof response.body.result.timeStamp).toStrictEqual("string");
+        expect(typeof response.body.result.blockMiner).toStrictEqual("string");
+        expect(typeof response.body.result.blockReward).toStrictEqual("string");
+        expect(typeof response.body.result.uncleInclusionReward).toStrictEqual("string");
+        expect(typeof response.body.result.uncles).toStrictEqual("object");
+      });
     });
   });
 });
