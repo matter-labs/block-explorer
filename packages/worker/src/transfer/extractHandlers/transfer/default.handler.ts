9047c937e89673957e037f99c90764d51854a5f0
Francisco Krause Arnim <fkrausear@gmail.com>
2024-01-29 17:29:48 -0300

    fix: worker tests

--- packages/worker/src/transfer/extractHandlers/transfer/default.handler.ts
+++ packages/worker/src/transfer/extractHandlers/transfer/default.handler.ts
@@ -6,7 +6,7 @@ import { TokenType } from "../../../entities/token.entity";
 import { unixTimeToDate } from "../../../utils/date";
 import parseLog from "../../../utils/parseLog";
 import { CONTRACT_INTERFACES } from "../../../constants";
-import { isNativeToken } from "src/common/token";
+import { isNativeToken } from "../../../../src/common/token";
 
 export const defaultTransferHandler: ExtractTransferHandler = {
   matches: (log: types.Log): boolean => log.topics.length === 3,
