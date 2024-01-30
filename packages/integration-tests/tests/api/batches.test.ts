fde87c429b16bb46bc2e384a95c42fe1dad997c7
Artem Melnytskyi <112873874+amelnytskyi@users.noreply.github.com>
2024-01-30 18:56:56 +0200

    test: covering Batches API and partially covered Transactions (#156)
    covering Batches API
    
    # What ❔
    
    1506 [Transactions] /transactions response returns elements (wasn't
    covered fully)
    1513 [Batches] /batches response returns elements (wasn't covered fully)
    1510 [Transactions] /address/{address}/logs response returns elements
    (wasn't covered fully)
    1656 [Transactions] Verify failed tx (new test)
    1655 [Transactions] Verify deployed the own ERC20 token contract in BE
    (new test)
    
    ## Why ❔
    
    Why are these changes done? - expanding test coverage
    
    ## Checklist
    
    - [ +] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ +] Tests for the changes have been added / updated.
    - [ n/a] Documentation comments have been added / updated.

--- packages/integration-tests/tests/api/batches.test.ts
+++ packages/integration-tests/tests/api/batches.test.ts
@@ -14,8 +14,26 @@ describe("Batches", () => {
       apiRoute = `/batches`;
       response = await helper.performGETrequest(apiRoute);
 
+      expect(response.status).toBe(200);
       expect(Array.isArray(response.body.items)).toStrictEqual(true);
       expect(response.body.items.length).toBeGreaterThanOrEqual(1);
+      expect(typeof response.body.items[0].number).toStrictEqual("number");
+      expect(response.body.items[0].number).toBeGreaterThanOrEqual(1);
+      expect(typeof response.body.items[0].timestamp).toStrictEqual("string");
+      expect(response.body.items[0].timestamp.length).toBe(24);
+      expect(typeof response.body.items[0].rootHash).toStrictEqual("string");
+      expect(response.body.items[0].rootHash.length).toBe(66);
+      expect(response.body.items[0].rootHash.startsWith("0x")).toBe(true);
+      expect(typeof response.body.items[0].executedAt).toStrictEqual("string");
+      expect(response.body.items[0].executedAt.length).toBe(24);
+      expect(typeof response.body.items[0].l1TxCount).toStrictEqual("number");
+      expect(response.body.items[0].l1TxCount).toBeGreaterThanOrEqual(0);
+      expect(typeof response.body.items[0].l2TxCount).toStrictEqual("number");
+      expect(response.body.items[0].l2TxCount).toBeGreaterThanOrEqual(0);
+      expect(typeof response.body.items[0].size).toStrictEqual("number");
+      expect(response.body.items[0].size).toBeGreaterThanOrEqual(0);
+      expect(typeof response.body.items[0].status).toStrictEqual("string");
+      expect(response.body.items[0].status.length).toBeGreaterThanOrEqual(6);
       expect(typeof response.body.meta.totalItems).toStrictEqual("number");
       expect(typeof response.body.meta.itemCount).toStrictEqual("number");
       expect(typeof response.body.meta.itemsPerPage).toStrictEqual("number");
