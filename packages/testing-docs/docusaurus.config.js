ee17988db46d02fb81b510d5b7890f42e821748f
otani <maksym.kryva@gmail.com>
2024-02-08 16:50:16 +0200

    re-configure build for use custom dns

--- packages/testing-docs/docusaurus.config.js
+++ packages/testing-docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://matter-labs.github.io/',
+  url: 'https://https://be-test-docs.zksync.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/be-test-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
