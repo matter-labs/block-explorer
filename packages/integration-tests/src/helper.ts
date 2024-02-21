dca8142432e9da8ac86e25cf30df8a76243ce952
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2024-02-21 11:45:37 +0200

    feat: integration tests

---
+++ packages/integration-tests/src/helper.ts
@@ -0,0 +1,89 @@
+import { execSync } from "child_process";
+import { ethers } from "ethers";
+import { promises as fs } from "fs";
+import * as path from "path";
+import * as request from "supertest";
+import { Provider } from "zksync-web3";
+
+import { environment, localConfig } from "./config";
+import { Logger } from "./entities";
+
+import type { BaseProvider } from "@ethersproject/providers/src.ts/base-provider";
+
+export class Helper {
+  async txHashLogger(txType: string, txValue: string, tokenName?: string) {
+    const logMessage = `TxHash for ${txType} ${Logger.textSeparator} ${txValue}`;
+
+    if (tokenName === undefined) {
+      return console.log(logMessage);
+    } else {
+      return console.log(logMessage, ` ${tokenName}`);
+    }
+  }
+
+  async executeScript(script: string) {
+    const output = execSync(script, { encoding: "utf-8" });
+
+    try {
+      console.log(`> Run NPM Script "${script}":\n`, output);
+      return output;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getStringFromFile(fileName: string) {
+    const absoluteRoute = path.join(__dirname, "..", fileName);
+
+    try {
+      return await fs.readFile(absoluteRoute, { encoding: "utf-8" });
+    } catch {
+      console.log(`There is no the expected file: ${fileName}`);
+    }
+  }
+
+  async getBalanceETH(walletAddress: string, layer: string) {
+    let network: string;
+    let provider: BaseProvider;
+    if (layer == "L1") {
+      network = localConfig.L1Network;
+      provider = ethers.getDefaultProvider(network);
+    } else if (layer == "L2") {
+      network = localConfig.L2Network;
+      provider = new Provider(network);
+    } else {
+      console.log(`Wrong layer: ${layer}`);
+    }
+    return ethers.utils.formatUnits(await provider.getBalance(walletAddress), "wei");
+  }
+
+  async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async performGETrequest(apiRoute: string) {
+    return request(environment.blockExplorerAPI).get(apiRoute);
+  }
+
+  /**
+   * A retry wrapper method to enhance test stability in API testing.
+   * Useful when API response fields may not immediately reflect the expected state,
+   * but can update to the correct response after a delay.
+   * Attempts to execute the action a specified number of times (defined in localConfig.maxAPIretries)
+   * with a delay between attempts (localConfig.intervalAPIretries).
+   * Throws an error if the action consistently fails after all retries.
+   */
+  async retryTestAction(action) {
+    for (let i = 0; i < localConfig.maxAPIretries; i++) {
+      try {
+        await action();
+        return;
+      } catch (error) {
+        if (i === localConfig.maxAPIretries - 1) {
+          throw error;
+        }
+        await this.delay(localConfig.intervalAPIretries);
+      }
+    }
+  }
+}
---
+++ packages/integration-tests/tests/api/tokens.test.ts
@@ -0,0 +1,162 @@
+import { localConfig } from "../../src/config";
+import { Buffer, Token, TransactionsType, Wallets } from "../../src/entities";
+import { Helper } from "../../src/helper";
+import { Playbook } from "../../src/playbook/playbook";
+
+describe("Tokens", () => {
+  jest.setTimeout(localConfig.standardTimeout);
+
+  const helper = new Helper();
+  const playbook = new Playbook();
+  const bufferFile = "src/playbook/";
+  let apiRoute: string;
+  let l2Token: string;
+  let txHash: string;
+  let response;
+
+  beforeAll(async () => {
+    l2Token = await helper.getStringFromFile(bufferFile + Buffer.L2);
+  });
+
+  describe("/tokens", () => {
+    //@id1508
+    it("Verify the response via /tokens", async () => {
+      await helper.retryTestAction(async () => {
+        const l2DepositedToken = await helper.getStringFromFile(bufferFile + Buffer.L2deposited);
+        const l1Token = await helper.getStringFromFile(bufferFile + Buffer.L1);
+        apiRoute = `/tokens`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body.items)).toStrictEqual(true);
+        expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ l2Address: l2DepositedToken }));
+        expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ l1Address: l1Token }));
+        expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ symbol: "L1" }));
+        expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ name: "L1 ERC20 token" }));
+        expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ decimals: 18 }));
+        expect(response.body.items[1]).toStrictEqual(expect.objectContaining({ l2Address: l2Token }));
+        expect(response.body.items[1]).toStrictEqual(expect.objectContaining({ l1Address: null }));
+        expect(response.body.items[1]).toStrictEqual(expect.objectContaining({ symbol: "L2" }));
+        expect(response.body.items[1]).toStrictEqual(expect.objectContaining({ name: "L2 ERC20 token" }));
+        expect(response.body.items[1]).toStrictEqual(expect.objectContaining({ decimals: 18 }));
+        expect(typeof response.body.meta.totalItems).toStrictEqual("number");
+        expect(typeof response.body.meta.itemCount).toStrictEqual("number");
+        expect(typeof response.body.meta.itemsPerPage).toStrictEqual("number");
+        expect(typeof response.body.meta.totalPages).toStrictEqual("number");
+        expect(typeof response.body.meta.currentPage).toStrictEqual("number");
+        expect(response.body.links.first).toStrictEqual("tokens?limit=10");
+        expect(response.body.links.previous).toStrictEqual("");
+        expect(typeof response.body.links.next).toStrictEqual("string");
+        expect(typeof response.body.links.last).toStrictEqual("string");
+      });
+    });
+
+    //@id1456
+    it("Verify deployed to L2 custom token via /tokens/{tokenAddress}", async () => {
+      await helper.retryTestAction(async () => {
+        apiRoute = `/tokens/${l2Token}`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual({
+          l2Address: l2Token,
+          l1Address: null,
+          liquidity: null,
+          usdPrice: null,
+          iconURL: null,
+          symbol: Token.customL2TokenSymbol,
+          name: Token.customL2TokenName,
+          decimals: Token.customL2TokenDecimals,
+        });
+      });
+    });
+
+    describe("/tokens/{address}/transfers", () => {
+      beforeAll(async () => {
+        txHash = await playbook.transferERC20("0.01", l2Token, "L2");
+        await playbook.deployViaPaymaster();
+        await playbook.usePaymaster();
+      });
+
+      //@id1448
+      it("Verify the custom ERC20 token transfer via /tokens/{address}/transfers", async () => {
+        await helper.retryTestAction(async () => {
+          apiRoute = `/tokens/${l2Token}/transfers?page=1&limit=10`;
+          response = await helper.performGETrequest(apiRoute);
+
+          expect(response.status).toBe(200);
+          expect(response.body.items[0].amount).toBe("10000000000000000");
+          expect(response.body.items[0].from).toBe(Wallets.richWalletAddress);
+          expect(response.body.items[0].to).toBe(Wallets.secondWalletAddress);
+          expect(response.body.items[0].token).toEqual(expect.objectContaining({ l2Address: l2Token }));
+          expect(response.body.items[0]).toEqual(expect.objectContaining({ transactionHash: txHash }));
+          expect(response.body.items[0]).toEqual(expect.objectContaining({ type: "transfer" }));
+        });
+      });
+
+      //@id1451
+      it("Verify the custom token includes paymaster transaction via /tokens/{address}/transfers", async () => {
+        await helper.retryTestAction(async () => {
+          l2Token = await helper.getStringFromFile(bufferFile + Buffer.customToken);
+          const emptyWallet = await helper.getStringFromFile(bufferFile + Buffer.emptyWalletAddress);
+          const paymaster = await helper.getStringFromFile(bufferFile + Buffer.paymaster);
+          txHash = await helper.getStringFromFile(bufferFile + Buffer.paymasterTx);
+          apiRoute = `/tokens/${l2Token}/transfers?page=1&limit=10`;
+          response = await helper.performGETrequest(apiRoute);
+
+          expect(response.status).toBe(200);
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ from: emptyWallet }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ to: paymaster }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ l2Address: l2Token }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ transactionHash: txHash }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ type: TransactionsType.transfer }));
+        });
+      });
+
+      //id1803
+      it("Verify the response via /tokens/{address}/transfers", async () => {
+        await helper.retryTestAction(async () => {
+          l2Token = await helper.getStringFromFile(bufferFile + Buffer.customToken);
+          const emptyWallet = await helper.getStringFromFile(bufferFile + Buffer.emptyWalletAddress);
+          const paymaster = await helper.getStringFromFile(bufferFile + Buffer.paymaster);
+          txHash = await helper.getStringFromFile(bufferFile + Buffer.paymasterTx);
+          const apiRoute = `/tokens/${l2Token}/transfers?page=1&limit=10`;
+          const decapitalizedTokenAddress = l2Token.toLowerCase();
+          response = await helper.performGETrequest(apiRoute);
+
+          expect(response.status).toBe(200);
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ from: emptyWallet }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ to: paymaster }));
+          expect(typeof response.body.items[0].blockNumber).toStrictEqual("number");
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ transactionHash: txHash }));
+          expect(typeof response.body.items[0].timestamp).toStrictEqual("string");
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ amount: "1" }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ tokenAddress: l2Token }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ type: TransactionsType.transfer }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ tokenType: "ERC20" }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ fields: null }));
+          expect(response.body.items[0]).toStrictEqual(expect.objectContaining({ isInternal: false }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ l2Address: l2Token }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ l1Address: null }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ symbol: "MyToken" }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ name: "MyToken" }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ decimals: 18 }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ usdPrice: null }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ liquidity: null }));
+          expect(response.body.items[0].token).toStrictEqual(expect.objectContaining({ iconURL: null }));
+          expect(typeof response.body.meta.totalItems).toStrictEqual("number");
+          expect(typeof response.body.meta.itemCount).toStrictEqual("number");
+          expect(typeof response.body.meta.itemsPerPage).toStrictEqual("number");
+          expect(typeof response.body.meta.totalPages).toStrictEqual("number");
+          expect(typeof response.body.meta.currentPage).toStrictEqual("number");
+          expect(response.body.links.first).toStrictEqual(`tokens/${decapitalizedTokenAddress}/transfers?limit=10`);
+          expect(response.body.links.previous).toStrictEqual("");
+          expect(response.body.links.next).toStrictEqual("");
+          expect(response.body.links.last).toStrictEqual(
+            `tokens/${decapitalizedTokenAddress}/transfers?page=1&limit=10`
+          );
+        });
+      });
+    });
+  });
+});
