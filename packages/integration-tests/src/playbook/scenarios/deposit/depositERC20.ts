4babcacb7c0c540a5cebeecbe01712f7dafdf566
amelnytskyi <a.melnytskyi@matterlabs.dev>
2024-03-04 19:08:36 +0200

    test: covering UI tests for Deposit
    Changed timeout value in Deposit UI tests
    Renamed variables to suggested values as per review results
    Removed obsolete console.log

--- packages/integration-tests/src/playbook/scenarios/deposit/depositERC20.ts
+++ packages/integration-tests/src/playbook/scenarios/deposit/depositERC20.ts
@@ -11,8 +11,8 @@ const syncProvider = new zksync.Provider(localConfig.L2Network);
 const ethProvider = ethers.getDefaultProvider(localConfig.L1Network);
 const syncWallet = new zksync.Wallet(localConfig.privateKey, syncProvider, ethProvider);
 const playbookRoot = "src/playbook";
-const bufferFile = playbookRoot + "/" + Buffer.L2deposited;
-const bufferDepositErc20File = playbookRoot + "/" + Buffer.txERC20Deposit;
+const bufferAddressL2DepositedFile = playbookRoot + "/" + Buffer.L2deposited;
+const bufferTxErc20DepositFile = playbookRoot + "/" + Buffer.txERC20Deposit;
 
 export const depositERC20 = async function (sum = "0.5", tokenAddress: string, units = 18) {
   const deposit = await syncWallet.deposit({
@@ -28,10 +28,10 @@ export const depositERC20 = async function (sum = "0.5", tokenAddress: string, u
 
   const l2TokenAddress = await syncProvider.l2TokenAddress(tokenAddress);
   console.log("L2 token address ", l2TokenAddress);
-  await fs.writeFile(bufferFile, l2TokenAddress);
   const txHash = await deposit.waitFinalize();
   await helper.txHashLogger(Logger.deposit, txHash.transactionHash, "ERC20 token");
-  await fs.writeFile(bufferDepositErc20File, txHash.transactionHash);
+  await fs.writeFile(bufferAddressL2DepositedFile, l2TokenAddress);
+  await fs.writeFile(bufferTxErc20DepositFile, txHash.transactionHash);
 
   return txHash;
 };
--- packages/integration-tests/tests/ui/deposit.spec.ts
+++ packages/integration-tests/tests/ui/deposit.spec.ts
@@ -4,30 +4,32 @@ import { config } from "./config";
 import { BlockExplorer, Buffer, Wallets } from "../../src/entities";
 import { Helper } from "../../src/helper";
 
+import type { Locator } from "@playwright/test";
+
 const bufferRoute = "src/playbook/";
 const helper = new Helper();
 let url: string;
 let bufferFile;
 let depositTxHash: string;
-let fromAddress: string;
+let initiatorAddress: string;
+let hash, initiatorAddressElement, ethValue, erc20Value: Locator;
 
 //@id1660
 test("Check Deposit ETH transaction on BE", async ({ page }) => {
   bufferFile = bufferRoute + Buffer.txEthDeposit;
   depositTxHash = await helper.getStringFromFile(bufferFile);
   url = BlockExplorer.baseUrl + `/tx/${depositTxHash}` + BlockExplorer.localNetwork;
-  fromAddress = Wallets.richWalletAddress;
-  console.log(url);
+  initiatorAddress = Wallets.richWalletAddress;
 
   await page.goto(url);
 
-  const hash = await page.locator(`//*[text()="Transaction Hash"]/..//..//*[text()="${depositTxHash}"]`).first();
-  const fromAddressElement = await page.locator(`text=${fromAddress}`).first();
-  const ethValue = await page.locator(`text=0.0000001`).first();
+  hash = await page.locator(`//*[text()="Transaction Hash"]/..//..//*[text()="${depositTxHash}"]`).first();
+  initiatorAddressElement = await page.locator(`text=${initiatorAddress}`).first();
+  ethValue = await page.locator(`text=0.0000001`).first();
 
-  await expect(hash).toBeVisible(config.extraTimeout);
-  await expect(ethValue).toBeVisible(config.extraTimeout);
-  await expect(fromAddressElement).toBeVisible(config.extraTimeout);
+  await expect(hash).toBeVisible(config.defaultTimeout);
+  await expect(ethValue).toBeVisible(config.defaultTimeout);
+  await expect(initiatorAddressElement).toBeVisible(config.defaultTimeout);
 });
 
 //@id1681
@@ -35,18 +37,17 @@ test("Check on BE Deposit the custom ERC-20 token", async ({ page }) => {
   bufferFile = bufferRoute + Buffer.txERC20Deposit;
   depositTxHash = await helper.getStringFromFile(bufferFile);
   url = BlockExplorer.baseUrl + `/tx/${depositTxHash}` + BlockExplorer.localNetwork;
-  fromAddress = Wallets.richWalletAddress;
-  console.log(url);
+  initiatorAddress = Wallets.richWalletAddress;
 
   await page.goto(url);
 
-  const hash = await page.locator(`//*[text()="Transaction Hash"]/..//..//*[text()="${depositTxHash}"]`).first();
-  const fromAddressElement = await page.locator(
+  hash = await page.locator(`//*[text()="Transaction Hash"]/..//..//*[text()="${depositTxHash}"]`).first();
+  initiatorAddressElement = await page.locator(
     `//*[text()="From"]/..//*[text()="L1"]/..//*[text()="0x36615Cf349d...c049"]`
   );
-  const erc20Value = await page.locator(`//*[text()="0x36615Cf349d...c049"]/..//..//*[text()="100"]`);
+  erc20Value = await page.locator(`//*[text()="0x36615Cf349d...c049"]/..//..//*[text()="100"]`);
 
-  await expect(hash).toBeVisible(config.extraTimeout);
-  await expect(erc20Value).toBeVisible(config.extraTimeout);
-  await expect(fromAddressElement).toBeVisible(config.extraTimeout);
+  await expect(hash).toBeVisible(config.defaultTimeout);
+  await expect(erc20Value).toBeVisible(config.defaultTimeout);
+  await expect(initiatorAddressElement).toBeVisible(config.defaultTimeout);
 });
