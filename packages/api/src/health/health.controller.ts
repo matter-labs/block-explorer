19b0099b4a9a8c1e3356d4a8e2d116e89c2f1d0c
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2024-03-06 14:31:54 +0200

    fix: add graceful shutdown timeout (#196)
    # What ❔
    
    Add graceful shutdown timeout.
    
    ## Why ❔
    
    To ensure that we can gracefully process all incoming HTTP requests
    before shutting down the service.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [X] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [X] Tests for the changes have been added / updated.

--- packages/api/src/health/health.controller.ts
+++ packages/api/src/health/health.controller.ts
@@ -1,18 +1,35 @@
-import { Controller, Get } from "@nestjs/common";
+import { Logger, Controller, Get, BeforeApplicationShutdown } from "@nestjs/common";
 import { HealthCheckService, TypeOrmHealthIndicator, HealthCheck, HealthCheckResult } from "@nestjs/terminus";
 import { ApiExcludeController } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
+import { setTimeout } from "node:timers/promises";
 
 @ApiExcludeController()
 @Controller(["health", "ready"])
-export class HealthController {
+export class HealthController implements BeforeApplicationShutdown {
+  private readonly logger: Logger;
+  private readonly gracefulShutdownTimeoutMs: number;
+
   constructor(
     private readonly healthCheckService: HealthCheckService,
-    private readonly dbHealthChecker: TypeOrmHealthIndicator
-  ) {}
+    private readonly dbHealthChecker: TypeOrmHealthIndicator,
+    configService: ConfigService
+  ) {
+    this.logger = new Logger(HealthController.name);
+    this.gracefulShutdownTimeoutMs = configService.get<number>("gracefulShutdownTimeoutMs");
+  }
 
   @Get()
   @HealthCheck()
   public async check(): Promise<HealthCheckResult> {
     return await this.healthCheckService.check([() => this.dbHealthChecker.pingCheck("database")]);
+  }
+
+  public async beforeApplicationShutdown(signal?: string): Promise<void> {
+    if (this.gracefulShutdownTimeoutMs && signal === "SIGTERM") {
+      this.logger.debug(`Awaiting ${this.gracefulShutdownTimeoutMs}ms before shutdown`);
+      await setTimeout(this.gracefulShutdownTimeoutMs);
+      this.logger.debug(`Timeout reached, shutting down now`);
+    }
   }
 }
