9beae69dae7cb83c46fdfdbc09b5a7d887f20a3d
Oleh Bairak <ob@matterlabs.dev>
2024-03-12 18:25:24 +0100

    fix: new one

--- packages/integration-tests/src/constants.ts
+++ packages/integration-tests/src/constants.ts
@@ -83,3 +83,7 @@ export enum BlockExplorer {
   baseUrl = "http://localhost:3010",
   localNetwork = "/?network=local",
 }
+
+export enum Values {
+  txSumETH = "0.000009",
+}
--- packages/integration-tests/tests/ui/transactions.spec.ts
+++ packages/integration-tests/tests/ui/transactions.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 import { config } from "./config";
-import { BlockExplorer, Buffer, Path } from "../../src/constants";
+import { BlockExplorer, Buffer, Path, Token, Values, Wallets } from "../../src/constants";
 import { Helper } from "../../src/helper";
 
 import type { Locator } from "@playwright/test";
@@ -10,6 +10,7 @@ const helper = new Helper();
 let url: string;
 let failedTxHash: string;
 let contract: string;
+let transaction: string;
 let element: Locator;
 let selector: string;
 
@@ -37,4 +38,82 @@ test("Verify deployed the own ERC20 token contract", async ({ page }) => {
   element = await page.locator(selector).first();
 
   await expect(element).toBeVisible(config.extraTimeout);
+});
+
+//@id1682
+test(" Check on BE Transfer ETH token via Portal", async ({ page }) => {
+  transaction = await helper.readFile(Path.absolutePathToBufferFiles, Buffer.txEthWithdraw);
+  url = BlockExplorer.baseUrl + `/tx/${transaction}` + BlockExplorer.localNetwork;
+
+  await page.goto(url);
+  //Check tx Hash
+  selector = `text=${transaction}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+  //Check address From
+  selector = `text=${Wallets.richWalletAddress}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+  //Check address To
+  selector = `text=${Token.ERC20AddressETH}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+
+  //Check transaction Amount
+  selector = `text=${Values.txSumETH}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+});
+
+//@id1680
+test(" Check on BE Transfer custom ERC-20 token via Portal", async ({ page }) => {
+  transaction = await helper.readFile(Path.absolutePathToBufferFiles, Buffer.txMultiTransferCustomTokenI);
+  const adressTo = await helper.readFile(Path.absolutePathToBufferFiles, Buffer.L2);
+  url = BlockExplorer.baseUrl + `/tx/${transaction}` + BlockExplorer.localNetwork;
+
+  await page.goto(url);
+  //Check tx Hash
+  selector = `text=${transaction}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+  //Check address From
+  selector = `text=${Wallets.richWalletAddress}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+  //Check address To
+  selector = `text=${adressTo}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+
+  //Check transaction amount
+  selector = `text= 1 `;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+});
+
+//@id1683
+test("Check on BE contract that makes multiple transfers based on stored/retrieved ETH + ERC20", async ({ page }) => {
+  //contract address
+  contract = await helper.readFile(Path.absolutePathToBufferFiles, Buffer.addressMultiTransferETH);
+  //tx hash from deployed contract
+  const txAddress = await helper.readFile(Path.absolutePathToBufferFiles, Buffer.txMultiTransferETH);
+  url = BlockExplorer.baseUrl + `/address/${contract}` + BlockExplorer.localNetwork;
+
+  await page.goto(url);
+  //Check contract address
+  selector = `text=${contract}`;
+  element = await page.locator(selector).first();
+
+  await expect(element).toBeVisible(config.extraTimeout);
+  //Check tx hash
+  selector = `text=${txAddress}`;
+  element = await page.locator(selector).first();
 });
