82b10ce16081df0bb504bdc2735e6f21bc6be76e
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2024-03-14 09:07:00 +0200

    fix: typo

--- packages/data-fetcher/src/utils/overrideBigNumberToJson.ts
+++ packages/data-fetcher/src/utils/overrideBigNumberToJson.ts
@@ -3,7 +3,7 @@ import { BigNumber } from "ethers";
 export default function overrideBigNumberToJson() {
   // override to customize JSON.stringify serialization for BigNumbers
   BigNumber.prototype.toJSON = function toJSON() {
-    // return BigNumber decimal representation
+    // return BigNumber string representation
     return this.toString();
   };
 }
