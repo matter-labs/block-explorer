3c4c5fc7aa9f861a320a5ade096cd6e0c707fa6f
Pavel <pavel.cheremushkin@gmail.com>
2024-07-10 13:26:11 +0200

    test: debug 0 balance on L1

--- packages/integration-tests/src/playbook/deploy/nftToL1.ts
+++ packages/integration-tests/src/playbook/deploy/nftToL1.ts
@@ -1,5 +1,5 @@
 import { promises as fs } from "fs";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import * as hardhatConfig from "hardhat";
 
 import { Buffer, Wallets } from "../../entities";
@@ -8,8 +8,17 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
 async function main() {
   const hre: HardhatRuntimeEnvironment = hardhatConfig;
-  const MyNFTArtifact = await hre.artifacts.readArtifact("MyNFT");
   const [deployer] = await ethers.getSigners();
+  const deployerBalance = await deployer.getBalance();
+
+  console.log("Network:", network.name);
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Deployer account balance:", deployerBalance.toString());
+
+  if (deployerBalance.lt(ethers.utils.parseEther("0.01"))) {
+    throw new Error(">>>Insufficient funds for gas * price + value");
+  }
+  const MyNFTArtifact = await hre.artifacts.readArtifact("MyNFT");
   const MyNFTFactory = new ethers.ContractFactory(MyNFTArtifact.abi, MyNFTArtifact.bytecode, deployer);
 
   const myNFT = await MyNFTFactory.deploy();
