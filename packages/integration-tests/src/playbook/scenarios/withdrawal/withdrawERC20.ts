d8b216e2f35ae4ada1b517edc5ce11489284f688
Pavel <pavel.cheremushkin@gmail.com>
2024-07-25 18:17:34 +0200

    test: first test run

--- packages/integration-tests/src/playbook/scenarios/withdrawal/withdrawERC20.ts
+++ packages/integration-tests/src/playbook/scenarios/withdrawal/withdrawERC20.ts
@@ -1,6 +1,6 @@
 import * as ethers from "ethers";
 import { promises as fs } from "fs";
-import * as zksync from "zksync-web3";
+import * as zksync from "zksync-ethers";
 
 import { localConfig } from "../../../config";
 import { Buffer, Logger, Wallets } from "../../../entities";
@@ -21,7 +21,7 @@ export const withdrawERC20 = async function (tokenAddress: string, sum = "0.2") 
 
   const withdrawL2 = await syncWallet.withdraw({
     to: Wallets.richWalletAddress,
-    amount: ethers.utils.parseEther(sum),
+    amount: ethers.parseEther(sum),
     token: tokenAddress,
     bridgeAddress: bridges.erc20L2,
     overrides: localConfig.gasLimit,
