c93290c329f835a1c101d174c00f2b213c02aae1
Pavel <pavel.cheremushkin@gmail.com>
2024-07-31 17:47:52 +0200

    test: test with and without a timeout

--- packages/integration-tests/tests/hooks/global.ts
+++ packages/integration-tests/tests/hooks/global.ts
@@ -1,9 +1,6 @@
 import { Buffer } from "../../src/entities";
 import { Helper } from "../../src/helper";
 import { Playbook } from "../../src/playbook/playbook";
-async function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
 export default async () => {
   const playbook = new Playbook();
@@ -11,11 +8,8 @@ export default async () => {
   const bufferRoute = "src/playbook/";
 
   await playbook.deployERC20toL2();
-  await sleep(2000);
   await playbook.deployERC20toL1();
-  await sleep(2000);
   await playbook.depositETH("0.0000001");
-  await sleep(2000);
   const bufferFile = bufferRoute + Buffer.L1;
   const token = await helper.getStringFromFile(bufferFile);
   await playbook.depositERC20("100", token, 18);
