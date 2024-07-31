bedc4066368fe1dfbe70cc56669db9444a6aa8cc
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2024-07-31 20:05:19 +0300

    fix: unit tests coverage

--- packages/api/src/config/index.spec.ts
+++ packages/api/src/config/index.spec.ts
@@ -203,6 +203,69 @@ describe("config", () => {
         });
       });
     });
+
+    describe("and only l2 eth address is provided", () => {
+      it("sets other l2 eth fields from the default configuration", () => {
+        process.env = {
+          BASE_TOKEN_SYMBOL: "MTTL",
+          BASE_TOKEN_DECIMALS: "18",
+          BASE_TOKEN_L1_ADDRESS: "0xSomeAddress",
+          BASE_TOKEN_ICON_URL: "https://matter-labs.io",
+          BASE_TOKEN_NAME: "MatterLabs",
+          NODE_ENV: "test",
+
+          ETH_TOKEN_L2_ADDRESS: "0x000000000000000000000000000000000000800A1",
+        };
+
+        expect(config()).toEqual({
+          baseToken: {
+            l2Address: "0x000000000000000000000000000000000000800A",
+            l1Address: "0xSomeAddress",
+            symbol: "MTTL",
+            name: "MatterLabs",
+            decimals: 18,
+            iconURL: "https://matter-labs.io",
+          },
+          ethToken: {
+            l2Address: "0x000000000000000000000000000000000000800A1",
+            l1Address: "0x0000000000000000000000000000000000000000",
+            liquidity: undefined,
+            name: "Ether",
+            symbol: "ETH",
+            usdPrice: undefined,
+            decimals: 18,
+            iconURL: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1698873266",
+          },
+          NODE_ENV: "test",
+          port: 3020,
+          metrics: {
+            port: 3005,
+            collectDbConnectionPoolMetricsInterval: 10000,
+          },
+          typeORM: {
+            type: "postgres",
+            url: "postgres://postgres:postgres@127.0.0.1:5432/block-explorer",
+            poolSize: 300,
+            extra: {
+              idleTimeoutMillis: 60000,
+              statement_timeout: 90000,
+            },
+            synchronize: true,
+            logging: false,
+            autoLoadEntities: true,
+            retryAttempts: 10,
+            retryDelay: 3000,
+            applicationName: "block-explorer-api",
+          },
+          contractVerificationApiUrl: "http://127.0.0.1:3070",
+          featureFlags: {
+            feature1Enabled: true,
+            feature2Enabled: false,
+          },
+          gracefulShutdownTimeoutMs: 0,
+        });
+      });
+    });
   });
 
   it("sets replication DB config if replica set exists", () => {
