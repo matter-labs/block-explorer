4faa3a76dddb30b677fc9e1b91d1d6c2a5da65d3
Pavel <pavel.cheremushkin@gmail.com>
2024-07-31 19:20:01 +0200

    test: refactoring

--- packages/integration-tests/src/playbook/scenarios/deploy/deployERC20toL2.ts
+++ packages/integration-tests/src/playbook/scenarios/deploy/deployERC20toL2.ts
@@ -8,10 +8,10 @@ export const deployERC20toL2 = async function () {
   const playbookRoot = path.join("src", "playbook");
   const bufferFile = path.join(playbookRoot, Buffer.L2);
   await helper.printLog(bufferFile);
-  await helper.executeScript(`cat ${bufferFile}`);
 
   await helper.executeScript(`cd ${playbookRoot} && npm run compile`);
   await helper.executeScript(`cd ${playbookRoot} && npm run deployToL2`);
+  await helper.executeScript(`cat ${bufferFile}`);
   await helper.printLog(`Return to the initial script: deployERC20toL2`);
   const deployedToken = await helper.getStringFromFile(bufferFile);
   await helper.printLog(`Before the latest PRINT in global`);
