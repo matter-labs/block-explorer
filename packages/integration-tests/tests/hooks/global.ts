a9defa527fe1815a1a39c84c62c38512b4e07150
Pavel <pavel.cheremushkin@gmail.com>
2024-08-14 14:02:31 +0200

    test: return global config back

--- packages/integration-tests/tests/hooks/global.ts
+++ packages/integration-tests/tests/hooks/global.ts
@@ -1,19 +1,20 @@
-// import { Buffer } from "../../src/entities";
-// import { Helper } from "../../src/helper";
+import * as path from "path";
+
+import { Buffer } from "../../src/entities";
+import { Helper } from "../../src/helper";
 import { Playbook } from "../../src/playbook/playbook";
 
 export default async () => {
-  console.log("A stub");
   const playbook = new Playbook();
-  // const helper = new Helper();
-  // const bufferRoute = "src/playbook/";
+  const helper = new Helper();
+  const bufferRoute = path.join("src", "playbook");
 
   // await playbook.compileContracts();
   await playbook.deployERC20toL2();
-  // await playbook.deployERC20toL1();
-  // await playbook.depositETH("0.0000001");
-  // const bufferFile = bufferRoute + Buffer.L1;
-  // const token = await helper.getStringFromFile(bufferFile);
-  // await playbook.depositERC20("100", token, 18);
+  await playbook.deployERC20toL1();
+  await playbook.depositETH("0.0000001");
+  const bufferFile = path.join(bufferRoute, Buffer.L1);
+  const token = await helper.getStringFromFile(bufferFile);
+  await playbook.depositERC20("100", token, 18);
   console.log("Final step in global == == ==");
 };
