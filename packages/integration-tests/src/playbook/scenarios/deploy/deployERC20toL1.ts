2111a9422a3576c095c4c649bd39e417508b5a01
Pavel <pavel.cheremushkin@gmail.com>
2024-08-20 12:52:18 +0200

    test: refactoring return lic check

--- packages/integration-tests/src/playbook/scenarios/deploy/deployERC20toL1.ts
+++ packages/integration-tests/src/playbook/scenarios/deploy/deployERC20toL1.ts
@@ -7,9 +7,8 @@ export const deployERC20toL1 = async function () {
   const bufferFile = playbookRoot + "/" + Buffer.L1;
 
   await helper.executeScript(`cd ${playbookRoot} && npm run compile`);
-  await helper.delay(1500);
   await helper.executeScript(`cd ${playbookRoot} && npm run deployToL1`);
-  await helper.delay(1500);
+
   const deployedToken = await helper.getStringFromFile(bufferFile);
   console.log("The custom ERC20 token has been deployed to L1: ", Logger.textSeparator, deployedToken);
 
