572e068702974111414d6d6b11368e246d7bece0
Oleh Bairak <ob@matterlabs.dev>
2024-08-22 18:42:14 +0500

    fix: added negative logs test cases

--- packages/integration-tests/tests/api/logs.test.ts
+++ packages/integration-tests/tests/api/logs.test.ts
@@ -39,7 +39,7 @@ describe("API module: Logs", () => {
         expect(response.body.result[0].data.length).toBe(194);
         expect(typeof response.body.result[0].blockNumber).toStrictEqual("string");
         expect(response.body.result[0].blockNumber.startsWith("0x")).toBe(true);
-        expect(response.body.result[0].blockNumber.length).toBe(3);
+        expect(response.body.result[0].blockNumber.length).toBe(4);
         expect(typeof response.body.result[0].timeStamp).toStrictEqual("string");
         expect(response.body.result[0].timeStamp.startsWith("0x")).toBe(true);
         expect(response.body.result[0].timeStamp.length).toBe(10);
@@ -56,6 +56,76 @@ describe("API module: Logs", () => {
         expect(typeof response.body.result[0].transactionIndex).toStrictEqual("string");
         expect(response.body.result[0].transactionIndex.startsWith("0x")).toBe(true);
         expect(response.body.result[0].transactionIndex.length).toBe(3);
+      });
+    });
+    //@id1953:I
+    it("Verify /api?module=logs&action=getLogs response - Invalid address format", async () => {
+      await helper.retryTestAction(async () => {
+        apiRoute = `/api?module=logs&action=getLogs&page=1&offset=10&toBlock=99999999&fromBlock=0&address=0x8A63F953e19aA4Ce3ED90621EeF61sE17A95c6594`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "0" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "NOTOK" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ result: "Error! Invalid address format" }));
+      });
+    });
+
+    //@id1953:II
+    it("Verify /api?module=logs&action=getLogs response - No record found", async () => {
+      await helper.retryTestAction(async () => {
+        apiRoute = `/api?module=logs&action=getLogs&page=1&offset=10&toBlock=99999999&fromBlock=0&address=0x8A63F953e19aA4Ce3ED90621EeF61E17A95c6594`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "0" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "No record found" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ result: [] }));
+      });
+    });
+
+    //@id1953:III
+    it("Verify /api?module=logs&action=getLogs response - Validation failed", async () => {
+      await helper.retryTestAction(async () => {
+        apiRoute = `/api?module=logs&action=getLogs&page=1&offset=10&toBlock=999999999999999999999999999&fromBlock=0&address=0xbf2A1ACE3B12b81bab4985f05E850AcFCCb416E0`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "0" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "NOTOK" }));
+        expect(response.body).toStrictEqual(
+          expect.objectContaining({ result: "Validation failed (numeric string is expected)" })
+        );
+      });
+    });
+
+    //@id1953:IV
+    it("Verify /api?module=logs&action=getLogs response - offset must not be less than 1", async () => {
+      await helper.retryTestAction(async () => {
+        apiRoute = `/api?module=logs&action=getLogs&page=1&offset=0&toBlock=999999999999&fromBlock=0&address=0xbf2A1ACE3B12b81bab4985f05E850AcFCCb416E0`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "0" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "NOTOK" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ result: "offset must not be less than 1" }));
+      });
+    });
+
+    //@id1953:V
+    it("Verify /api?module=logs&action=getLogs response - Validation failed: specified int is out of defined boundaries", async () => {
+      await helper.retryTestAction(async () => {
+        apiRoute = `/api?module=logs&action=getLogs&page=1&offset=10&toBlock=999999999999&fromBlock=-1&address=0xbf2A1ACE3B12b81bab4985f05E850AcFCCb416E0`;
+        response = await helper.performGETrequest(apiRoute);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toStrictEqual(expect.objectContaining({ status: "0" }));
+        expect(response.body).toStrictEqual(expect.objectContaining({ message: "NOTOK" }));
+        expect(response.body).toStrictEqual(
+          expect.objectContaining({
+            result: "Validation failed: specified int is out of defined boundaries: [0;9007199254740991].",
+          })
+        );
       });
     });
   });
