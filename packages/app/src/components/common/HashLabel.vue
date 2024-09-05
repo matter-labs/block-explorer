bf3f1269ea452759039e9828a7669ea4293fe81b
mm <mm@matterlabs.dev>
2024-09-05 09:44:46 +0200

    chore: more hacks

--- packages/app/src/components/common/HashLabel.vue
+++ packages/app/src/components/common/HashLabel.vue
@@ -9,6 +9,60 @@
 </template>
 
 <script lang="ts">
+function hexToAscii(hex: string): string {
+  let asciiStr = "";
+
+  // Make sure hex length is even
+  if (hex.length % 2 !== 0) {
+    throw new Error("Invalid hex string");
+  }
+
+  for (let i = 0; i < hex.length; i += 2) {
+    // Convert each pair of hex digits to an ASCII character
+    const hexByte = hex.slice(i, i + 2);
+    const charCode = parseInt(hexByte, 16);
+    asciiStr += String.fromCharCode(charCode);
+  }
+
+  return asciiStr;
+}
+function parseEthereumAddress(address: string): string | null {
+  // Check if the address starts with "0x"
+  if (!address.startsWith("0x")) {
+    return null;
+    //throw new Error("Invalid Ethereum address format. Address should start with '0x'.");
+  }
+
+  // Remove the "0x" prefix
+  const cleanAddress = address.slice(2);
+
+  // Check if the first 20 characters (10 bytes) are all zeroes
+  const zeroBytes = "00000000000000000000"; // 20 characters of zero
+
+  if (cleanAddress.startsWith(zeroBytes)) {
+    // Extract the rest of the address after the 10 zero bytes
+    let restOfAddress = cleanAddress.slice(20);
+    // Remove leading "00" hex values from the rest of the address
+    restOfAddress = restOfAddress.replace(/^0+/, "");
+    restOfAddress = restOfAddress.toLowerCase();
+
+    // Convert the remaining part of the address from hex to ASCII
+    try {
+      const asciiString = hexToAscii(restOfAddress);
+      // System contract
+      if (asciiString.length < 3) {
+        return null;
+      }
+      return asciiString;
+    } catch (error) {
+      console.error("Failed to parse the remaining address as hex ASCII.");
+      return null;
+    }
+  }
+
+  return null; // If the address doesn't start with 10 bytes of zeroes
+}
+
 export function shortenFitText(
   text: string,
   placement = "middle",
@@ -16,6 +70,10 @@ export function shortenFitText(
   singleCharacterWidth = 6,
   subtraction = 3
 ): string {
+  if (parseEthereumAddress(text) != null) {
+    text = parseEthereumAddress(text)!;
+  }
+
   const indexEnd =
     rootWidth && singleCharacterWidth ? Math.floor(rootWidth / singleCharacterWidth / 2) - subtraction : 13;
 
