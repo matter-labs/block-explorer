4e46784747bb8b2075e6766402f1406c0dfba9c6
calvo.generico <miguelfeliped@gmail.com>
2024-11-01 15:56:06 -0300

    feat(proxy): forward transaction endpoints
    Added blind forward for transaction endpoints to unblock front end development.

--- packages/proxy/src/routes/index.ts
+++ packages/proxy/src/routes/index.ts
@@ -4,6 +4,7 @@ import authRoutes from './auth.js';
 import { blocksRoutes } from './blocks-bff.js';
 import { batchRoutes } from './batches-bff.js';
 import { statsRoutes } from './stats-bff.js';
+import { transationsRoutes } from './transactions-bff.js';
 
 export const allRoutes = (app: FastifyApp) => {
   app.register(addressRoutes, { prefix: '/address' });
@@ -11,4 +12,5 @@ export const allRoutes = (app: FastifyApp) => {
   app.register(blocksRoutes, { prefix: '/blocks' });
   app.register(batchRoutes, { prefix: '/batches' });
   app.register(statsRoutes, { prefix: '/stats' });
+  app.register(transationsRoutes, { prefix: '/transactions' });
 };
