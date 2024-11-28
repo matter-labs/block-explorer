65df16a40438e45a0aa97550b6d70bde50b15a1a
MexicanAce <nicolasvillanueva@msn.com>
2024-11-28 06:43:08 -0600

    fix: lint

--- packages/app/src/views/BatchesView.vue
+++ packages/app/src/views/BatchesView.vue
@@ -18,7 +18,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { computed, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
 
