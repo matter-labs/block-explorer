5ef08c77e0a09b1bf9cf7c486b3eeed2a8e9ed94
aon <21188659+aon@users.noreply.github.com>
2025-05-07 13:16:50 -0300

    feat: extract private validium config

--- packages/api/src/config/featureFlags.ts
+++ packages/api/src/config/featureFlags.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-const { DISABLE_API_SCHEMA_DOCS, DISABLE_BFF_API_SCHEMA_DOCS, DISABLE_EXTERNAL_API, DOUBLE_ZERO } = process.env;
+const { DISABLE_API_SCHEMA_DOCS, DISABLE_BFF_API_SCHEMA_DOCS, DISABLE_EXTERNAL_API, PRIVATE_VALIDIUM } = process.env;
 
 export const swagger = {
   enabled: DISABLE_API_SCHEMA_DOCS !== "true",
@@ -10,4 +10,4 @@ export const swagger = {
 
 export const disableExternalAPI = DISABLE_EXTERNAL_API === "true";
 
-export const doubleZero = DOUBLE_ZERO === "true";
+export const privateValidium = PRIVATE_VALIDIUM === "true";
