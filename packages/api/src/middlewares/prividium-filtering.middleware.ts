4da01bf1fe89eb9f3745d2366acccfcd459443f2
aon <21188659+aon@users.noreply.github.com>
2025-05-13 10:13:13 -0300

    feat: change private validium to prividium

--- packages/api/src/middlewares/prividium-filtering.middleware.ts
+++ packages/api/src/middlewares/prividium-filtering.middleware.ts
@@ -7,7 +7,7 @@ import { FilterTransactionsOptions } from "../transaction/transaction.service";
 const OWNERSHIP_TRANSFERRED_TOPIC = "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0";
 
 @Injectable()
-export class PrivateFilteringMiddleware implements NestMiddleware {
+export class PrividiumFilteringMiddleware implements NestMiddleware {
   constructor(private readonly addressService: AddressService, private readonly logService: LogService) {}
 
   public async use(req: Request, res: Response, next: NextFunction) {
