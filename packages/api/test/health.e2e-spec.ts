c6a38fdfd3e0329691f76a5e9a2c63136c1a2e35
aon <21188659+aon@users.noreply.github.com>
2025-05-13 12:50:21 -0300

    fix: imports with new tsconfig

--- packages/api/test/health.e2e-spec.ts
+++ packages/api/test/health.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
-import * as request from "supertest";
+import request from "supertest";
 import { AppModule } from "../src/app.module";
 import { configureApp } from "../src/configureApp";
 
