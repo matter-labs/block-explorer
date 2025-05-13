81593ac2403af2125be1b52ce78ff35c41cf4ecc
aon <21188659+aon@users.noreply.github.com>
2025-05-13 12:50:21 -0300

    fix: imports with new tsconfig

--- packages/api/test/contract-api.e2e-spec.ts
+++ packages/api/test/contract-api.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import { getRepositoryToken } from "@nestjs/typeorm";
-import * as request from "supertest";
+import request from "supertest";
 import { Repository } from "typeorm";
-import * as nock from "nock";
+import nock from "nock";
 import { Address } from "../src/address/address.entity";
 import { AppModule } from "../src/app.module";
 import { configureApp } from "../src/configureApp";
