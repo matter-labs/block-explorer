6d59ae59d8226f56c90c224755fa85e44b310514
Jrigada <jrigada@frba.utn.edu.ar>
2025-05-15 14:59:47 -0300

    feat: add check for logout disconecting wallet

--- packages/app/src/components/ConnectMetamaskButton.vue
+++ packages/app/src/components/ConnectMetamaskButton.vue
@@ -30,7 +30,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
 
@@ -98,9 +98,9 @@ watch(
   }
 );
 
-if (address.value) {
+onMounted(() => {
   updateChainId();
-}
+});
 
 const isWrongNetwork = computed(() => {
   if (!currentChainId.value) return false;
