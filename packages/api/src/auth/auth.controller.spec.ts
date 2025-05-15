6a1d16cfa552b970fc58ba7856f71e87889b3727
calvo.generico <miguelfeliped@gmail.com>
2025-05-14 21:15:37 -0300

    chore: tests auth controller
    WIP

--- packages/api/src/auth/auth.controller.spec.ts
+++ packages/api/src/auth/auth.controller.spec.ts
@@ -2,81 +2,14 @@ import { AuthController } from "./auth.controller";
 import { mock } from "jest-mock-extended";
 import { Request } from "express";
 import { VerifySignatureDto } from "./auth.dto";
-import { BadRequestException, HttpException, UnprocessableEntityException } from "@nestjs/common";
-import { Wallet } from "zksync-ethers";
-import { SiweMessage } from "siwe";
-import { ConfigService } from "@nestjs/config";
-import { SiweMessage } from "siwe";
-
-type CalculatedSiwe = {
-  msg: string;
-  signature: string;
-  siwe: SiweMessage;
-  address: string;
-};
-
-async function calculateSiwe({
-  nonce,
-  privateKey,
-  expiresAt = null,
-  scheme = "http",
-  domain = "localhost",
-}: {
-  nonce: string;
-  privateKey: string;
-  expiresAt?: null | Date;
-  scheme?: "http" | "https";
-  domain?: string;
-}): Promise<CalculatedSiwe> {
-  // Create account from private key
-  const account = new Wallet(privateKey);
-
-  // Create SIWE message
-  const message = new SiweMessage({
-    domain,
-    address: account.address,
-    statement: "Sign in with Ethereum",
-    uri: `${scheme}://${domain}`,
-    version: "1",
-    chainId: 1,
-    nonce,
-    scheme,
-  });
-
-  if (expiresAt !== null) {
-    message.expirationTime = expiresAt.toISOString();
-  }
-
-  // Create message string and sign it
-  const messageString = message.prepareMessage();
-  const signature = await account.signMessage(messageString);
-  return {
-    msg: messageString,
-    signature,
-    siwe: message,
-    address: account.address,
-  };
-}
+import { BadRequestException } from "@nestjs/common";
 
 describe("AuthController", () => {
   let controller: AuthController;
   let req: Request;
-  let configServiceMock: ConfigService;
 
   beforeEach(() => {
-    configServiceMock = mock<ConfigService>({
-      get: jest.fn().mockImplementation((key: string) => {
-        switch (key) {
-          case "NODE_ENV":
-            return "production";
-          case "appHostname":
-            return "blockexplorer.com";
-          default:
-            return undefined;
-        }
-      }),
-    });
-    controller = new AuthController(configServiceMock);
+    controller = new AuthController();
     req = mock<Request>();
   });
 
@@ -87,9 +20,9 @@ describe("AuthController", () => {
     });
 
     it("returns a new nonce each time", async () => {
-      const nonce1 = controller.getNonce(req);
-      const nonce2 = controller.getNonce(req);
-      const nonce3 = controller.getNonce(req);
+      const nonce1 = await controller.getNonce(req);
+      const nonce2 = await controller.getNonce(req);
+      const nonce3 = await controller.getNonce(req);
 
       expect(nonce1).not.toEqual(nonce2);
       expect(nonce1).not.toEqual(nonce3);
@@ -97,14 +30,14 @@ describe("AuthController", () => {
     });
 
     it("sets the new nonce in the cookie", async () => {
-      const nonce = controller.getNonce(req);
+      const nonce = await controller.getNonce(req);
 
       expect(req.session.nonce).toEqual(nonce);
     });
 
     it("replaces old nonde with new nonce each time", async () => {
-      const nonce1 = controller.getNonce(req);
-      const nonce2 = controller.getNonce(req);
+      const nonce1 = await controller.getNonce(req);
+      const nonce2 = await controller.getNonce(req);
 
       expect(nonce1).not.toEqual(nonce2);
       expect(req.session.nonce).toEqual(nonce2);
@@ -112,7 +45,7 @@ describe("AuthController", () => {
   });
 
   describe("verifySignature", () => {
-    let body: VerifySignatureDto;
+    let body;
     beforeEach(() => {
       body = new VerifySignatureDto();
     });
@@ -121,262 +54,31 @@ describe("AuthController", () => {
       await expect(() => controller.verifySignature(body, req)).rejects.toThrow(BadRequestException);
     });
 
-    it("validates for http in development", async () => {
-      const configServiceMock = mock<ConfigService>({
-        get: jest.fn().mockImplementation((key: string) => {
-          switch (key) {
-            case "NODE_ENV":
-              return "development";
-            case "appHostname":
-              return "blockexplorer.com";
-            default:
-              return undefined;
-          }
-        }),
-      });
-      const controller = new AuthController(configServiceMock);
-
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-      req.session = {
-        nonce: nonce,
-      };
-
-      const { msg, signature } = await calculateSiwe({
-        nonce,
-        privateKey,
-        domain: "blockexplorer.com",
-        scheme: "http",
-      });
-
-      body.message = msg;
-      body.signature = signature;
-
-      const res = await controller.verifySignature(body, req);
-      expect(res).toBe(true);
-    });
-
     it("returns true when a correctly signed message is send", async () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
       req.session = {
-        nonce: nonce,
+        nonce: "8r2cXq20yD3l5bomR",
       };
-
-      const {
-        msg,
-        signature,
-        address,
-        siwe: originalSiwe,
-      } = await calculateSiwe({
-        nonce,
-        privateKey,
-        domain: "blockexplorer.com",
-        scheme: "https",
-      });
-
-      body.message = msg;
-      body.signature = signature;
+      body.message =
+        "localhost wants you to sign in with your Ethereum account:\n0x36Ea1B6673eA6269014D6cA0AdCca6598f618319\n\nSign in with Ethereum\n\nURI: http://localhost:3000\nVersion: 1\nChain ID: 1\nNonce: 8r2cXq20yD3l5bomR\nIssued At: 2025-05-15T00:07:06.055Z";
+      body.signature =
+        "0xc8db1d7c3e28fc25444fb1365457213f5026039905d20ff45651d2227fadd87d7fc3378d7e8cd8a9e072b649844e2082bb145f38b6ff2e5e987f8c1ff9d4a7361c";
 
       const res = await controller.verifySignature(body, req);
       expect(res).toBe(true);
       expect(req.session.siwe).not.toBe(undefined);
-      expect(req.session.siwe.address).toBe(address);
-      expect(req.session.siwe.uri).toBe(originalSiwe.uri);
-      expect(req.session.siwe.chainId).toBe(originalSiwe.chainId);
-      expect(req.session.siwe.domain).toBe(originalSiwe.domain);
-      expect(req.session.siwe.scheme).toBe(originalSiwe.scheme);
     });
 
-    it("throws error and set session to null when a nonce does not match", async () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    it("returns false and set session to null when a signature cannot be verified", async () => {
       req.session = {
-        nonce: nonce,
+        nonce: "8r2cXq20yD3l5bomR",
       };
-
-      const { msg, signature } = await calculateSiwe({ nonce, privateKey });
-
-      body.message = msg.replace(nonce, "falsenonce");
-      body.signature = signature;
+      body.message =
+        "localhost wants you to sign in with your Ethereum account:\n0x36Ea1B6673eA6269014D6cA0AdCca6598f618319\n\nSign in with Ethereum\n\nURI: http://localhost:3000\nVersion: 1\nChain ID: 1\nNonce: 8r2cXq20yD3l5bomR\nIssued At: 2025-05-15T00:07:06.055Z";
+      body.signature =
+        "0xc8db1d7c3e28fc25444fb1365457213f5026039905d20ff45651d2227fadd87d7fc3378d7e8cd8a9e072b649844e2082bb145f38b6ff2e5e987f8c1ff9d4a7361b";
 
       await expect(() => controller.verifySignature(body, req)).rejects.toThrow(BadRequestException);
       expect(req.session).toBe(null);
-    });
-
-    it("throws when msg is not a correct siwe message", async () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-      req.session = {
-        nonce: nonce,
-      };
-
-      const { signature } = await calculateSiwe(nonce, privateKey);
-
-      body.message = "badmsg";
-      body.signature = signature;
-
-      await expect(() => controller.verifySignature(body, req)).rejects.toThrow(BadRequestException);
-      expect(req.session).toBe(null);
-    });
-
-    it("throws error and set session to null when a signature cannot be verified", async () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-      req.session = {
-        nonce: nonce,
-      };
-
-      const { msg, signature } = await calculateSiwe({
-        nonce,
-        privateKey,
-        domain: "blockexplorer.com",
-        scheme: "https",
-      });
-      const badSig = Buffer.from(signature.replace("0x", ""), "hex");
-      badSig[badSig.length - 1] = ~badSig[badSig.length - 1] & 0xff; // change 1 byte
-
-      body.message = msg;
-      body.signature = `0x${badSig.toString("hex")}`;
-
-      await expect(() => controller.verifySignature(body, req)).rejects.toThrow(UnprocessableEntityException);
-      expect(req.session).toBe(null);
-    });
-
-    it("throws error and set session to null when message is expired", async () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-      req.session = {
-        nonce: nonce,
-      };
-
-      const expiresAt = new Date();
-      expiresAt.setFullYear(expiresAt.getFullYear() - 1); // One year ago;
-      const { msg, signature } = await calculateSiwe({
-        scheme: "https",
-        domain: "blockexplorer.com",
-        nonce,
-        privateKey,
-        expiresAt,
-      });
-
-      body.message = msg;
-      body.signature = signature;
-
-      await expect(() => controller.verifySignature(body, req)).rejects.toThrow(
-        new HttpException({ message: "Expired message." }, 440)
-      );
-      expect(req.session).toBe(null);
-    });
-  });
-
-  describe("logout", () => {
-    it("sets session to null", () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      req.session = {
-        nonce: nonce,
-      };
-      controller.logout(req);
-      expect(req.session).toEqual(null);
-    });
-  });
-
-  describe("GET /me", () => {
-    it("returns address stored in session", async () => {
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-      const { siwe, address } = await calculateSiwe({ nonce, privateKey });
-      req.session.siwe = siwe;
-      const res = await controller.me(req);
-      expect(res).toEqual({ address });
-    });
-  });
-
-  describe("POST /token", () => {
-    let originalFetch: typeof fetch;
-    let mockFetch: ReturnType<typeof jest.fn>;
-    let siwe: SiweMessage;
-
-    beforeEach(async () => {
-      originalFetch = global.fetch;
-      const mock = jest.fn();
-      mockFetch = mock;
-      global.fetch = mock;
-      const nonce = "8r2cXq20yD3l5bomR";
-      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-      const { siwe: siweMsg } = await calculateSiwe({ nonce, privateKey });
-      siwe = siweMsg;
-    });
-
-    afterEach(() => {
-      global.fetch = originalFetch;
-    });
-
-    it("fails if rpc returns with an error", async () => {
-      const rpcResponse = {
-        ok: true,
-        json: () =>
-          Promise.resolve({
-            token: null,
-            ok: false,
-          }),
-      };
-
-      req.session.siwe = siwe;
-      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
-
-      await expect(() => controller.token(req)).rejects.toThrow(
-        new BadRequestException({ message: "Failed to generate token" })
-      );
-    });
-
-    it("returns correct response if rpc returns correct value", async () => {
-      const rpcResponse = {
-        ok: true,
-        json: () =>
-          Promise.resolve({
-            token: "sometoken",
-            ok: true,
-          }),
-      };
-
-      req.session.siwe = siwe;
-
-      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
-      const res = await controller.token(req);
-      expect(res).toEqual({ ok: true, token: "sometoken" });
-    });
-
-    it("fails if fetch returns non ok response", async () => {
-      const rpcResponse = {
-        ok: false,
-        json: () =>
-          Promise.resolve({
-            token: "sometoken",
-            ok: true,
-          }),
-      };
-
-      req.session.siwe = siwe;
-
-      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
-      await expect(() => controller.token(req)).rejects.toThrow(new HttpException("Error creating token", 424));
-    });
-
-    it("fails if fetch returns wrong response format", async () => {
-      const rpcResponse = {
-        ok: true,
-        json: () =>
-          Promise.resolve({
-            token2: "sometoken",
-            ok: true,
-          }),
-      };
-
-      req.session.siwe = siwe;
-
-      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
-      await expect(() => controller.token(req)).rejects.toThrow(new BadRequestException("Failed to generate token"));
     });
   });
 });
