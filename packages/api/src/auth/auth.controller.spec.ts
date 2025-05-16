dfaf3126c2497a9eb4b12dc770a710895368b43c
calvo.generico <miguelfeliped@gmail.com>
2025-05-16 15:09:47 -0300

    chore: coverage reached

--- packages/api/src/auth/auth.controller.spec.ts
+++ packages/api/src/auth/auth.controller.spec.ts
@@ -5,6 +5,7 @@ import { VerifySignatureDto } from "./auth.dto";
 import { BadRequestException, HttpException, UnprocessableEntityException } from "@nestjs/common";
 import { calculateSiwe } from "../../test/utils/siwe-message-tools";
 import { ConfigService } from "@nestjs/config";
+import { SiweMessage } from "siwe";
 
 describe("AuthController", () => {
   let controller: AuthController;
@@ -35,9 +36,9 @@ describe("AuthController", () => {
     });
 
     it("returns a new nonce each time", async () => {
-      const nonce1 = await controller.getNonce(req);
-      const nonce2 = await controller.getNonce(req);
-      const nonce3 = await controller.getNonce(req);
+      const nonce1 = controller.getNonce(req);
+      const nonce2 = controller.getNonce(req);
+      const nonce3 = controller.getNonce(req);
 
       expect(nonce1).not.toEqual(nonce2);
       expect(nonce1).not.toEqual(nonce3);
@@ -45,14 +46,14 @@ describe("AuthController", () => {
     });
 
     it("sets the new nonce in the cookie", async () => {
-      const nonce = await controller.getNonce(req);
+      const nonce = controller.getNonce(req);
 
       expect(req.session.nonce).toEqual(nonce);
     });
 
     it("replaces old nonde with new nonce each time", async () => {
-      const nonce1 = await controller.getNonce(req);
-      const nonce2 = await controller.getNonce(req);
+      const nonce1 = controller.getNonce(req);
+      const nonce2 = controller.getNonce(req);
 
       expect(nonce1).not.toEqual(nonce2);
       expect(req.session.nonce).toEqual(nonce2);
@@ -67,6 +68,41 @@ describe("AuthController", () => {
 
     it("throws when there is no nonce", async () => {
       await expect(() => controller.verifySignature(body, req)).rejects.toThrow(BadRequestException);
+    });
+
+    it("validates for http in development", async () => {
+      const configServiceMock = mock<ConfigService>({
+        get: jest.fn().mockImplementation((key: string) => {
+          switch (key) {
+            case "NODE_ENV":
+              return "development";
+            case "appHostname":
+              return "blockexplorer.com";
+            default:
+              return undefined;
+          }
+        }),
+      });
+      const controller = new AuthController(configServiceMock);
+
+      const nonce = "8r2cXq20yD3l5bomR";
+      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+      req.session = {
+        nonce: nonce,
+      };
+
+      const { msg, signature } = await calculateSiwe({
+        nonce,
+        privateKey,
+        domain: "blockexplorer.com",
+        scheme: "http",
+      });
+
+      body.message = msg;
+      body.signature = signature;
+
+      const res = await controller.verifySignature(body, req);
+      expect(res).toBe(true);
     });
 
     it("returns true when a correctly signed message is send", async () => {
@@ -165,12 +201,20 @@ describe("AuthController", () => {
 
       const expiresAt = new Date();
       expiresAt.setFullYear(expiresAt.getFullYear() - 1); // One year ago;
-      const { msg, signature } = await calculateSiwe({ nonce, privateKey, expiresAt });
+      const { msg, signature } = await calculateSiwe({
+        scheme: "https",
+        domain: "blockexplorer.com",
+        nonce,
+        privateKey,
+        expiresAt,
+      });
 
       body.message = msg;
       body.signature = signature;
 
-      await expect(() => controller.verifySignature(body, req)).rejects.toThrow(HttpException);
+      await expect(() => controller.verifySignature(body, req)).rejects.toThrow(
+        new HttpException({ message: "Expired message." }, 440)
+      );
       expect(req.session).toBe(null);
     });
   });
@@ -186,7 +230,7 @@ describe("AuthController", () => {
     });
   });
 
-  describe("me", () => {
+  describe("GET /me", () => {
     it("returns address stored in session", async () => {
       const nonce = "8r2cXq20yD3l5bomR";
       const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
@@ -194,6 +238,94 @@ describe("AuthController", () => {
       req.session.siwe = siwe;
       const res = await controller.me(req);
       expect(res).toEqual({ address });
+    });
+  });
+
+  describe("POST /token", () => {
+    let originalFetch: typeof fetch;
+    let mockFetch: ReturnType<typeof jest.fn>;
+    let siwe: SiweMessage;
+
+    beforeEach(async () => {
+      originalFetch = global.fetch;
+      const mock = jest.fn();
+      mockFetch = mock;
+      global.fetch = mock;
+      const nonce = "8r2cXq20yD3l5bomR";
+      const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+      const { siwe: siweMsg } = await calculateSiwe({ nonce, privateKey });
+      siwe = siweMsg;
+    });
+
+    afterEach(() => {
+      global.fetch = originalFetch;
+    });
+
+    it("fails if rpc returns with an error", async () => {
+      const rpcResponse = {
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            token: null,
+            ok: false,
+          }),
+      };
+
+      req.session.siwe = siwe;
+      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
+
+      await expect(() => controller.token(req)).rejects.toThrow(
+        new BadRequestException({ message: "Failed to generate token" })
+      );
+    });
+
+    it("returns correct response if rpc returns correct value", async () => {
+      const rpcResponse = {
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            token: "sometoken",
+            ok: true,
+          }),
+      };
+
+      req.session.siwe = siwe;
+
+      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
+      const res = await controller.token(req);
+      expect(res).toEqual({ ok: true, token: "sometoken" });
+    });
+
+    it("fails if fetch returns non ok response", async () => {
+      const rpcResponse = {
+        ok: false,
+        json: () =>
+          Promise.resolve({
+            token: "sometoken",
+            ok: true,
+          }),
+      };
+
+      req.session.siwe = siwe;
+
+      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
+      await expect(() => controller.token(req)).rejects.toThrow(new HttpException("Error creating token", 424));
+    });
+
+    it("fails if fetch returns wrong response format", async () => {
+      const rpcResponse = {
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            token2: "sometoken",
+            ok: true,
+          }),
+      };
+
+      req.session.siwe = siwe;
+
+      mockFetch.mockReturnValue(Promise.resolve(rpcResponse));
+      await expect(() => controller.token(req)).rejects.toThrow(new BadRequestException("Failed to generate token"));
     });
   });
 });
