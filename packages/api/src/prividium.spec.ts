bb2ab25bf5766a84964e26d51d8be965a5ff70d2
calvo.generico <miguelfeliped@gmail.com>
2025-05-16 16:14:27 -0300

    chore: avoid using global config for secrets

--- packages/api/src/prividium.spec.ts
+++ packages/api/src/prividium.spec.ts
@@ -11,7 +11,7 @@ import { PrividiumFilteringMiddleware } from "./middlewares/prividium-filtering.
 describe("applyPrividiumExpressConfig", () => {
   it("allows to set cookies", async () => {
     const app = express();
-    applyPrividiumExpressConfig(app as unknown as NestExpressApplication);
+    applyPrividiumExpressConfig(app as unknown as NestExpressApplication, "secretvalue");
     const nonce = "somenonce";
     app.get("/test", (req, res) => {
       req.session.nonce = nonce;
