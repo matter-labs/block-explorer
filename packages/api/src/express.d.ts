4340d019e7bbfacddcab82dec3ba7409c65ec3b2
calvo.generico <miguelfeliped@gmail.com>
2025-05-21 18:05:07 -0300

    fix: improved res.locals types

--- packages/api/src/express.d.ts
+++ packages/api/src/express.d.ts
@@ -1,14 +1,16 @@
 import "express";
 import { FilterTransactionsOptions } from "./transaction/transaction.service";
+import { FilterTransfersOptions } from "./transfer/transfer.service";
+import { FilterLogsOptions } from "./log/log.service";
 
 declare module "express" {
   export interface Response {
     locals: {
       filterTransactionsOptions?: FilterTransactionsOptions;
-      filterAddressLogsOptions?: { visibleBy?: string };
-      filterAddressTransferOptions?: { transactingWith?: string };
+      filterAddressLogsOptions?: FilterLogsOptions;
+      filterAddressTransferOptions?: FilterTransfersOptions;
       filterAddressOptions?: { includeBalances?: boolean };
-      tokenTransfersOptions?: { address?: string };
+      tokenTransfersOptions?: FilterTransfersOptions;
     };
   }
 }
