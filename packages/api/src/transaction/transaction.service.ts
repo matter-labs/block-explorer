ce4133ad272979f976a40cf247ca9740e4a7eb35
romsters <petriv.roma@gmail.com>
2025-07-14 19:05:24 +0300

    fix: move prividium specific query to a separate block, rollback tests

--- packages/api/src/transaction/transaction.service.ts
+++ packages/api/src/transaction/transaction.service.ts
@@ -10,6 +10,7 @@ import { Batch } from "../batch/batch.entity";
 import { CounterService } from "../counter/counter.service";
 import { Log } from "../log/log.entity";
 import { hexTransformer } from "../common/transformers/hex.transformer";
+import { prividium } from "../config/featureFlags";
 import { zeroPadValue } from "ethers";
 
 export interface FilterTransactionsOptions {
@@ -61,128 +62,163 @@ export class TransactionService {
     paginationOptions: IPaginationOptions
   ): Promise<Pagination<Transaction>> {
     if (filterOptions.address) {
-      const queryBuilder = this.transactionRepository.createQueryBuilder("transaction");
+      // TODO: fix the query performance for prividium case and add tests
+      /* istanbul ignore if */ // this comment is to exclude the next block from code cov
+      if (prividium) {
+        const queryBuilder = this.transactionRepository.createQueryBuilder("transaction");
 
-      const commonParams: Record<string, string | number | Date> = {
-        address: hexTransformer.to(filterOptions.address),
-        ...(filterOptions.blockNumber !== undefined && { blockNumber: filterOptions.blockNumber }),
-        ...(filterOptions.l1BatchNumber !== undefined && { l1BatchNumber: filterOptions.l1BatchNumber }),
-        ...(filterOptions.visibleBy !== undefined && { visibleBy: hexTransformer.to(filterOptions.visibleBy) }),
-      };
+        const commonParams: Record<string, string | number | Date> = {
+          address: hexTransformer.to(filterOptions.address),
+          ...(filterOptions.blockNumber !== undefined && { blockNumber: filterOptions.blockNumber }),
+          ...(filterOptions.l1BatchNumber !== undefined && { l1BatchNumber: filterOptions.l1BatchNumber }),
+          ...(filterOptions.visibleBy !== undefined && { visibleBy: hexTransformer.to(filterOptions.visibleBy) }),
+        };
 
-      // FindOperator doesn't work with this query, so we need to build the filter manually
-      let receivedAtFilter: string | undefined = undefined;
-      if (filterOptions.receivedAt !== undefined) {
-        switch (filterOptions.receivedAt.type) {
-          case "between":
-            receivedAtFilter = `BETWEEN :receivedAtStart AND :receivedAtEnd`;
-            commonParams.receivedAtStart = filterOptions.receivedAt.value[0];
-            commonParams.receivedAtEnd = filterOptions.receivedAt.value[1];
-            break;
-          case "moreThanOrEqual":
-            receivedAtFilter = `>= :receivedAt`;
-            commonParams.receivedAt = filterOptions.receivedAt.value;
-            break;
-          case "lessThanOrEqual":
-            receivedAtFilter = `<= :receivedAt`;
-            commonParams.receivedAt = filterOptions.receivedAt.value;
-            break;
-          default:
-            throw new Error(`Unsupported FindOperator type: ${filterOptions.receivedAt.type}`);
+        // FindOperator doesn't work with this query, so we need to build the filter manually
+        let receivedAtFilter: string | undefined = undefined;
+        if (filterOptions.receivedAt !== undefined) {
+          switch (filterOptions.receivedAt.type) {
+            case "between":
+              receivedAtFilter = `BETWEEN :receivedAtStart AND :receivedAtEnd`;
+              commonParams.receivedAtStart = filterOptions.receivedAt.value[0];
+              commonParams.receivedAtEnd = filterOptions.receivedAt.value[1];
+              break;
+            case "moreThanOrEqual":
+              receivedAtFilter = `>= :receivedAt`;
+              commonParams.receivedAt = filterOptions.receivedAt.value;
+              break;
+            case "lessThanOrEqual":
+              receivedAtFilter = `<= :receivedAt`;
+              commonParams.receivedAt = filterOptions.receivedAt.value;
+              break;
+            default:
+              throw new Error(`Unsupported FindOperator type: ${filterOptions.receivedAt.type}`);
+          }
         }
-      }
 
-      const addressTransactionSubQuery =
-        this.addressTransactionRepository.createQueryBuilder("sub1_addressTransaction");
-      addressTransactionSubQuery.select("sub1_addressTransaction.transactionHash");
-      addressTransactionSubQuery.innerJoin("sub1_addressTransaction.transaction", "sub1_transaction");
-      addressTransactionSubQuery.where("sub1_addressTransaction.address = :address");
+        const addressTransactionSubQuery =
+          this.addressTransactionRepository.createQueryBuilder("sub1_addressTransaction");
+        addressTransactionSubQuery.select("sub1_addressTransaction.transactionHash");
+        addressTransactionSubQuery.innerJoin("sub1_addressTransaction.transaction", "sub1_transaction");
+        addressTransactionSubQuery.where("sub1_addressTransaction.address = :address");
 
-      if (filterOptions.blockNumber !== undefined) {
-        addressTransactionSubQuery.andWhere("sub1_transaction.blockNumber = :blockNumber");
-      }
-      if (filterOptions.l1BatchNumber !== undefined) {
-        addressTransactionSubQuery.andWhere("sub1_transaction.l1BatchNumber = :l1BatchNumber");
-      }
-      if (filterOptions.receivedAt !== undefined) {
-        addressTransactionSubQuery.andWhere(`sub1_transaction.receivedAt ${receivedAtFilter}`);
-      }
-      // If `visibleBy` is provided, we filter by transactions that are
-      // from or to `visibleBy` address in addition to being from or to `address`
-      if (filterOptions.visibleBy !== undefined) {
-        addressTransactionSubQuery.andWhere(
-          new Brackets((qb) => {
-            qb.where("sub1_transaction.from = :visibleBy");
-            qb.orWhere("sub1_transaction.to = :visibleBy");
-          })
-        );
-      }
-
-      // If `filterAddressInLogTopics` is provided, we filter by log topics where
-      // the address is mentioned
-      if (filterOptions.filterAddressInLogTopics) {
-        const logSubQuery = this.logRepository.createQueryBuilder("sub2_log");
-        logSubQuery.select("sub2_log.transactionHash");
-        logSubQuery.innerJoin("sub2_log.transaction", "sub2_transaction");
-
-        logSubQuery.where(
-          new Brackets((qb) => {
-            qb.where("sub2_log.topics[1] = :paddedAddressBytes");
-            qb.orWhere("sub2_log.topics[2] = :paddedAddressBytes");
-            qb.orWhere("sub2_log.topics[3] = :paddedAddressBytes");
-          })
-        );
-        // If `visibleBy` is provided, in addition to filtering by log topics,
-        // we filter by transactions that were from or to `address`
+        if (filterOptions.blockNumber !== undefined) {
+          addressTransactionSubQuery.andWhere("sub1_transaction.blockNumber = :blockNumber");
+        }
+        if (filterOptions.l1BatchNumber !== undefined) {
+          addressTransactionSubQuery.andWhere("sub1_transaction.l1BatchNumber = :l1BatchNumber");
+        }
+        if (filterOptions.receivedAt !== undefined) {
+          addressTransactionSubQuery.andWhere(`sub1_transaction.receivedAt ${receivedAtFilter}`);
+        }
+        // If `visibleBy` is provided, we filter by transactions that are
+        // from or to `visibleBy` address in addition to being from or to `address`
         if (filterOptions.visibleBy !== undefined) {
-          logSubQuery.andWhere(
+          addressTransactionSubQuery.andWhere(
             new Brackets((qb) => {
-              qb.where("sub2_transaction.from = :address");
-              qb.orWhere("sub2_transaction.to = :address");
+              qb.where("sub1_transaction.from = :visibleBy");
+              qb.orWhere("sub1_transaction.to = :visibleBy");
             })
           );
         }
 
-        if (filterOptions.blockNumber !== undefined) {
-          logSubQuery.andWhere("sub2_transaction.blockNumber = :blockNumber");
-        }
-        if (filterOptions.l1BatchNumber !== undefined) {
-          logSubQuery.andWhere("sub2_transaction.l1BatchNumber = :l1BatchNumber");
-        }
-        if (filterOptions.receivedAt !== undefined) {
-          logSubQuery.andWhere(`sub2_transaction.receivedAt ${receivedAtFilter}`);
+        // If `filterAddressInLogTopics` is provided, we filter by log topics where
+        // the address is mentioned
+        if (filterOptions.filterAddressInLogTopics) {
+          const logSubQuery = this.logRepository.createQueryBuilder("sub2_log");
+          logSubQuery.select("sub2_log.transactionHash");
+          logSubQuery.innerJoin("sub2_log.transaction", "sub2_transaction");
+
+          logSubQuery.where(
+            new Brackets((qb) => {
+              qb.where("sub2_log.topics[1] = :paddedAddressBytes");
+              qb.orWhere("sub2_log.topics[2] = :paddedAddressBytes");
+              qb.orWhere("sub2_log.topics[3] = :paddedAddressBytes");
+            })
+          );
+          // If `visibleBy` is provided, in addition to filtering by log topics,
+          // we filter by transactions that were from or to `address`
+          if (filterOptions.visibleBy !== undefined) {
+            logSubQuery.andWhere(
+              new Brackets((qb) => {
+                qb.where("sub2_transaction.from = :address");
+                qb.orWhere("sub2_transaction.to = :address");
+              })
+            );
+          }
+
+          if (filterOptions.blockNumber !== undefined) {
+            logSubQuery.andWhere("sub2_transaction.blockNumber = :blockNumber");
+          }
+          if (filterOptions.l1BatchNumber !== undefined) {
+            logSubQuery.andWhere("sub2_transaction.l1BatchNumber = :l1BatchNumber");
+          }
+          if (filterOptions.receivedAt !== undefined) {
+            logSubQuery.andWhere(`sub2_transaction.receivedAt ${receivedAtFilter}`);
+          }
+
+          // If `visibleBy` is provided, we use it to filter log topics given we want to see transactions
+          // from `visibleBy` perspective
+          const logAddressParam = {
+            paddedAddressBytes: hexTransformer.to(zeroPadValue(filterOptions.visibleBy || filterOptions.address, 32)),
+          };
+
+          queryBuilder.where(`transaction.hash IN (
+            (${addressTransactionSubQuery.getQuery()})
+            UNION
+            (${logSubQuery.getQuery()})
+          )`);
+          queryBuilder.setParameters({
+            ...commonParams,
+            ...logAddressParam,
+          });
+        } else {
+          queryBuilder.where(`transaction.hash IN (${addressTransactionSubQuery.getQuery()})`);
+          queryBuilder.setParameters(commonParams);
         }
 
-        // If `visibleBy` is provided, we use it to filter log topics given we want to see transactions
-        // from `visibleBy` perspective
-        const logAddressParam = {
-          paddedAddressBytes: hexTransformer.to(zeroPadValue(filterOptions.visibleBy || filterOptions.address, 32)),
-        };
+        queryBuilder.leftJoin("transaction.transactionReceipt", "transactionReceipt");
+        queryBuilder.addSelect(["transactionReceipt.gasUsed", "transactionReceipt.contractAddress"]);
+        queryBuilder.leftJoin("transaction.batch", "batch");
+        queryBuilder.addSelect(["batch.commitTxHash", "batch.executeTxHash", "batch.proveTxHash"]);
 
-        queryBuilder.where(`transaction.hash IN (
-          (${addressTransactionSubQuery.getQuery()})
-          UNION
-          (${logSubQuery.getQuery()})
-        )`);
-        queryBuilder.setParameters({
-          ...commonParams,
-          ...logAddressParam,
-        });
-      } else {
-        queryBuilder.where(`transaction.hash IN (${addressTransactionSubQuery.getQuery()})`);
-        queryBuilder.setParameters(commonParams);
+        queryBuilder.orderBy("transaction.blockNumber", "DESC");
+        queryBuilder.addOrderBy("transaction.receivedAt", "DESC");
+        queryBuilder.addOrderBy("transaction.transactionIndex", "DESC");
+
+        return await paginate<Transaction>(queryBuilder, paginationOptions);
       }
 
+      const queryBuilder = this.addressTransactionRepository.createQueryBuilder("addressTransaction");
+      queryBuilder.select("addressTransaction.number");
+      queryBuilder.leftJoinAndSelect("addressTransaction.transaction", "transaction");
       queryBuilder.leftJoin("transaction.transactionReceipt", "transactionReceipt");
       queryBuilder.addSelect(["transactionReceipt.gasUsed", "transactionReceipt.contractAddress"]);
       queryBuilder.leftJoin("transaction.batch", "batch");
       queryBuilder.addSelect(["batch.commitTxHash", "batch.executeTxHash", "batch.proveTxHash"]);
-
-      queryBuilder.orderBy("transaction.blockNumber", "DESC");
-      queryBuilder.addOrderBy("transaction.receivedAt", "DESC");
-      queryBuilder.addOrderBy("transaction.transactionIndex", "DESC");
-
-      return await paginate<Transaction>(queryBuilder, paginationOptions);
+      queryBuilder.where({
+        address: filterOptions.address,
+        ...(filterOptions.receivedAt && { receivedAt: filterOptions.receivedAt }),
+        // can't add filters on transaction here due to typeOrm issue with filters on joined tables
+      });
+      if (filterOptions.blockNumber !== undefined) {
+        // can't use filters on transaction as object here due to typeOrm issue with filters on joined tables
+        queryBuilder.andWhere("transaction.blockNumber = :blockNumber", { blockNumber: filterOptions.blockNumber });
+      }
+      if (filterOptions.l1BatchNumber !== undefined) {
+        // can't use filters on transaction as object here due to typeOrm issue with filters on joined tables
+        queryBuilder.andWhere("transaction.l1BatchNumber = :l1BatchNumber", {
+          l1BatchNumber: filterOptions.l1BatchNumber,
+        });
+      }
+      queryBuilder.orderBy("addressTransaction.blockNumber", "DESC");
+      queryBuilder.addOrderBy("addressTransaction.receivedAt", "DESC");
+      queryBuilder.addOrderBy("addressTransaction.transactionIndex", "DESC");
+      const addressTransactions = await paginate<AddressTransaction>(queryBuilder, paginationOptions);
+      return {
+        ...addressTransactions,
+        items: addressTransactions.items.map((item) => item.transaction),
+      };
     } else {
       const queryBuilder = this.transactionRepository.createQueryBuilder("transaction");
       queryBuilder.leftJoin("transaction.transactionReceipt", "transactionReceipt");
