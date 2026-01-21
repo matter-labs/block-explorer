30eb8c03db3b1d70718c345b223b98a2942ba720
romsters <petriv.roma@gmail.com>
2026-01-21 12:09:03 +0200

    feat: update front-end image and config

--- packages/app/src/utils/formatters.ts
+++ packages/app/src/utils/formatters.ts
@@ -67,7 +67,7 @@ export function formatAddressFromHash(value: Hash) {
   const validValue = value.slice(0, 2) === "0x" ? value.slice(2) : value;
 
   if (validValue.length !== 64) {
-    throw new Error("Invalid hash");
+    return "";
   }
 
   return `0x${validValue.slice(24)}`;
