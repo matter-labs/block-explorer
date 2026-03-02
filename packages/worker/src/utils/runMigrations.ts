07607e9ba007fba93f6be9c7116b0146eeaa10b1
Roman Petriv <petriv.roma@gmail.com>
2026-03-02 15:07:53 +0200

    fix: use advisory lock for migrations (#588)
    # What ❔
    
    - use advisory lock for migrations
    
    ## Why ❔
    
    - using advisory lock is a better approach because it drops the lock
    even if the app crashes as connection drops.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ +] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ +] Tests for the changes have been added / updated.

--- packages/worker/src/utils/runMigrations.ts
+++ packages/worker/src/utils/runMigrations.ts
@@ -3,25 +3,48 @@ import { Logger } from "@nestjs/common";
 import { setTimeout } from "timers/promises";
 
 const MIGRATIONS_LOCK_CHECK_INTERVAL = 10000;
-const CREATE_LOCK_TABLE_SQL = "CREATE TABLE _lock (id bigint NOT NULL, PRIMARY KEY (id))";
-const DROP_LOCK_TABLE_SQL = "DROP TABLE _lock";
+const ADVISORY_LOCK_KEY = 1234567; // arbitrary fixed key for migration lock
 
 export default async (connection: DataSource, logger: Logger) => {
+  let queryRunner = null;
+
   while (true) {
     try {
-      await connection.query(CREATE_LOCK_TABLE_SQL);
-      break;
+      if (!queryRunner) {
+        queryRunner = connection.createQueryRunner();
+        await queryRunner.connect();
+      }
     } catch (err) {
+      logger.warn("Failed to connect to database, retrying...", err);
+      queryRunner = null;
+      await setTimeout(MIGRATIONS_LOCK_CHECK_INTERVAL);
+      continue;
+    }
+
+    try {
+      const [{ pg_try_advisory_lock: acquired }] = await queryRunner.query("SELECT pg_try_advisory_lock($1)", [
+        ADVISORY_LOCK_KEY,
+      ]);
+
+      if (acquired) {
+        break;
+      }
+
       logger.warn("Database is locked for migrations. Waiting for release...");
+      await setTimeout(MIGRATIONS_LOCK_CHECK_INTERVAL);
+    } catch (err) {
+      logger.warn("Failed to acquire migration lock, retrying...", err);
+      queryRunner = null;
       await setTimeout(MIGRATIONS_LOCK_CHECK_INTERVAL);
     }
   }
 
+  logger.log("Lock acquired, running migrations if there are any to run");
   try {
-    logger.log("_lock created, running migrations if there are any to run");
     await connection.runMigrations();
   } finally {
-    await connection.query(DROP_LOCK_TABLE_SQL);
-    logger.log("_lock removed, migrations completed");
+    await queryRunner.query("SELECT pg_advisory_unlock($1)", [ADVISORY_LOCK_KEY]);
+    await queryRunner.release();
+    logger.log("Lock released, migrations completed");
   }
 };
